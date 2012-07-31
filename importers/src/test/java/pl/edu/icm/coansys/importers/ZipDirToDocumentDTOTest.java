e64e775d6b33e65a87ca7b3c4e86094998ae29a8
pdendek <pdendek@icm.edu.pl>
2012-07-31 17:07:19 +0200

    tests for classes extracting metadata via ymodel

---
+++ importers/src/test/java/pl/edu/icm/coansys/importers/ZipDirToDocumentDTOTest.java
@@ -0,0 +1,33 @@
+/*
+ * (C) 2010-2012 ICM UW. All rights reserved.
+ */
+
+
+package pl.edu.icm.coansys.importers;
+
+import org.junit.Test;
+
+import pl.edu.icm.coansys.importers.iterators.ZipDirToDocumentDTOIterator;
+import pl.edu.icm.coansys.importers.model.DocumentDTO;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Artur Czeczko a.czeczko@icm.edu.pl
+ */
+public class ZipDirToDocumentDTOTest {
+    
+    @Test
+    public void readZipDirTest() {
+        String zipDirPath = this.getClass().getClassLoader().getResource("zipdir").getPath();
+        ZipDirToDocumentDTOIterator zdtp = new ZipDirToDocumentDTOIterator(zipDirPath, "TEST_COLLECTION");
+        long start = System.nanoTime();
+        int counter = 0;
+        for (DocumentDTO doc : zdtp) {
+            System.out.println("counter: " + counter++);
+            System.out.println("doc: " + doc.toString());
+            assertEquals(doc.getDocumentMetadata().getCollection(), "TEST_COLLECTION");
+        }
+        System.out.println(((System.nanoTime()-start)/1000000000)+"sec.");
+    }
+}
