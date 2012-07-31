544c2cba17efa466af1fca0c66ef454c30b810af
pdendek <pdendek@icm.edu.pl>
2012-07-31 16:55:56 +0200

    new classes for creating Hbase-oriented input

---
+++ importers/src/main/java/pl/edu/icm/coansys/importers/constants/HBaseConstant.java
@@ -0,0 +1,19 @@
+/*
+ * (C) 2010-2012 ICM UW. All rights reserved.
+ */
+
+
+package pl.edu.icm.coansys.importers.constants;
+
+/**
+ * 
+ * @author pdendek
+ *
+ */
+public class HBaseConstant {
+	public static final String familyMetadata = "m";
+	public static final String familyContent = "c";
+	
+	public static final String familyMetadataQualifierProto = "mproto";
+	public static final String familyContentQualifierProto = "cproto"; 
+}
