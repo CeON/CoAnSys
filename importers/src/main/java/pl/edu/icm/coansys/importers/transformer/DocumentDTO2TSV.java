039acc0d9d666301768898f1e8253b2bf6b78d77
pdendek <pdendek@icm.edu.pl>
2012-08-01 12:54:25 +0200

    Introducing DocumentDTO2TSV class

---
+++ importers/src/main/java/pl/edu/icm/coansys/importers/transformer/DocumentDTO2TSV.java
@@ -0,0 +1,31 @@
+/*
+ * (C) 2010-2012 ICM UW. All rights reserved.
+ */
+
+
+package pl.edu.icm.coansys.importers.transformer;
+
+import pl.edu.icm.coansys.importers.model.DocumentDTO;
+
+/**
+ * @author pdendek
+ */
+public class DocumentDTO2TSV {
+	
+	public static String translate(DocumentDTO docDTO) {
+		return composeRow(docDTO) +"\t"+ docDTO.getDocumentMetadata() +"\t"+docDTO.getMediaConteiner() +"\n";
+	}
+
+	private static String composeRow(DocumentDTO docDTO) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(docDTO.getCollection());
+//		sb.append(docDTO.getYear());
+		if(docDTO.getMediaTypes().size()>0)sb.append(1);
+		else sb.append(0);
+		sb.append("_");
+		sb.append(docDTO.getKey());
+		
+		return sb.toString();
+	}
+
+}
