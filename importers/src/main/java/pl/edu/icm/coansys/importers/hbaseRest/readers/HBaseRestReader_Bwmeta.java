1aef83d87d55fc61988e06b7e062dda96fb3aa41
pdendek <pdendek@icm.edu.pl>
2012-08-03 11:52:26 +0200

    replacing toy applications of the main function by operating on real data inserted by a hbaserestreader_bwmeta object

--- importers/src/main/java/pl/edu/icm/coansys/importers/hbaseRest/readers/HBaseRestReader_Bwmeta.java
+++ importers/src/main/java/pl/edu/icm/coansys/importers/hbaseRest/readers/HBaseRestReader_Bwmeta.java
@@ -5,6 +5,10 @@
 package pl.edu.icm.coansys.importers.hbaseRest.readers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Put;
@@ -21,7 +25,10 @@ import pl.edu.icm.coansys.importers.constants.HBaseConstant;
 import pl.edu.icm.coansys.importers.hbaseRest.model_toy.AddressBookProtos.Person;
 import pl.edu.icm.coansys.importers.iterators.ZipDirToDocumentDTOIterator;
 import pl.edu.icm.coansys.importers.model.DocumentDTO;
+import pl.edu.icm.coansys.importers.model.DocumentProtos.Author;
 import pl.edu.icm.coansys.importers.model.DocumentProtos.DocumentMetadata;
+import pl.edu.icm.coansys.importers.model.DocumentProtos.Media;
+import pl.edu.icm.coansys.importers.model.DocumentProtos.MediaConteiner;
 import pl.edu.icm.coansys.importers.transformer.DocumentDto2HBasePut;
 
 /**
@@ -32,10 +39,18 @@ import pl.edu.icm.coansys.importers.transformer.DocumentDto2HBasePut;
 public class HBaseRestReader_Bwmeta {
 	
 	public static void main(String[] args) throws IOException{
-		readDocumentMetadataHBase("localhost", 8080, "test");
+		HashMap<String, List<String>> rowAuthorsMap = readAuthorsFromDocumentMetadataHBase("localhost", 8080, "testProto");
+//		HashMap<String, List<String>> rowAuthorsMap = readPdfsFromDocumentMetadataHBase("localhost", 8080, "testProto");
+		
+		for(Entry<String, List<String>> e : rowAuthorsMap.entrySet()){
+			for(String an : e.getValue()){
+				System.out.println(e.getKey()+"\t\t"+an);
+			}
+		}
+		
 	}
-	
-	public static void readDocumentMetadataHBase(String remoteHost, int remotePort, String remoteTable) throws IOException{
+
+	public static HashMap<String, List<String>> readPdfsFromDocumentMetadataHBase(String remoteHost, int remotePort, String remoteTable) throws IOException{
 
 		RemoteHTable table = new RemoteHTable(
         		new Client(
@@ -43,48 +58,63 @@ public class HBaseRestReader_Bwmeta {
         		), remoteTable
         	);
 		
-		Person john =
-            Person.newBuilder().setId(1234).setName("John Doe").setEmail("jdoe@example.com").addPhone(
-            Person.PhoneNumber.newBuilder().setNumber("555-4321").setType(Person.PhoneType.HOME)).build();
-
-		System.out.println("============ inside code ============");
-    	byte[] value = john.toByteArray(); 
-    	Person p = Person.parseFrom(value);
-    	System.out.println(p);
-    	System.out.println("============ from hbase ============");
 		Scan scan = new Scan();
-        ResultScanner scanner = table.getScanner(scan);
+        ResultScanner scanner = table.getScanner(Bytes.toBytes(HBaseConstant.familyContent), Bytes.toBytes(HBaseConstant.familyContentQualifierProto));
         
-        
+        HashMap<String, List<String>> rowAuthorsMap = new HashMap<String, List<String>>(); 
         
         try {
             for (Result scannerResult : scanner) {
-                if(scannerResult.getValue(Bytes.toBytes("data"), Bytes.toBytes("3")) != null) {
-                    System.out.println("Scan: " + Person.parseFrom(scannerResult.value()));
-                    System.out.println("!!!! Is it equals?: "+john.equals(Person.parseFrom(scannerResult.value())));
-                }
-                else {
-                    System.out.println("Scan: " + Bytes.toString(scannerResult.value()));
+            	String rowId = new String(scannerResult.getRow());
+            	ArrayList<String> names = new ArrayList<String>();
+            	
+            	if(scannerResult.getValue(Bytes.toBytes(HBaseConstant.familyContent), Bytes.toBytes(HBaseConstant.familyContentQualifierProto)) != null) {
+            		MediaConteiner mc = MediaConteiner.parseFrom(scannerResult.value());
+            		for(Media media : mc.getMediaList()){
+            			names.add(media.getMediaType());
+            		}
+            		rowAuthorsMap.put(rowId, names);
+                }else {
+                    System.out.println("Parsing problem occured on row "+rowId);
                 }
             }
         } finally {
             scanner.close();
         }
-//		
-//		Scan scan = new Scan();
-//        ResultScanner scanner = table.getScanner(scan);
-//        try {
-//            for (Result scannerResult : scanner) {
-//                if(scannerResult.getValue(Bytes.toBytes(HBaseConstant.familyMetadata), 
-//                		Bytes.toBytes(HBaseConstant.familyMetadataQualifierProto)) != null) {
-//                    System.out.println("Scan: " + DocumentMetadata.parseFrom(scannerResult.value()));
-//                }
-//                else {
-//                    System.out.println("Scan: " + Bytes.toString(scannerResult.value()));
-//                }
-//            }
-//        } finally {
-//            scanner.close();
-//        }
+        return rowAuthorsMap;
+	}	
+	
+	public static HashMap<String, List<String>> readAuthorsFromDocumentMetadataHBase(String remoteHost, int remotePort, String remoteTable) throws IOException{
+
+		RemoteHTable table = new RemoteHTable(
+        		new Client(
+        				new Cluster().add(remoteHost, remotePort)
+        		), remoteTable
+        	);
+		
+		Scan scan = new Scan();
+        ResultScanner scanner = table.getScanner(Bytes.toBytes(HBaseConstant.familyMetadata), Bytes.toBytes(HBaseConstant.familyMetadataQualifierProto));
+        
+        HashMap<String, List<String>> rowAuthorsMap = new HashMap<String, List<String>>(); 
+        
+        try {
+            for (Result scannerResult : scanner) {
+            	String rowId = new String(scannerResult.getRow());
+            	ArrayList<String> names = new ArrayList<String>();
+            	
+            	if(scannerResult.getValue(Bytes.toBytes(HBaseConstant.familyMetadata), Bytes.toBytes(HBaseConstant.familyMetadataQualifierProto)) != null) {
+            		DocumentMetadata dm = DocumentMetadata.parseFrom(scannerResult.value());
+            		for(Author a : dm.getAuthorList()){
+            			names.add(a.getForenames() + " " + a.getSurname());
+            		}
+            		rowAuthorsMap.put(rowId, names);
+                }else {
+                    System.out.println("Parsing problem occured on row "+rowId);
+                }
+            }
+        } finally {
+            scanner.close();
+        }
+        return rowAuthorsMap;
 	}
 }
