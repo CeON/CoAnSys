f32358b8aefc68c91f9590a092bfbf8001aca56d
pdendek <pdendek@icm.edu.pl>
2012-08-22 15:55:14 +0200

    unified way to compose row id accross different writers

---
+++ importers/src/main/java/pl/edu/icm/coansys/importers/transformers/RowComposer.java
@@ -0,0 +1,23 @@
+package pl.edu.icm.coansys.importers.transformers;
+
+import pl.edu.icm.coansys.importers.constants.HBaseConstant;
+import pl.edu.icm.coansys.importers.constants.ProtoConstants;
+import pl.edu.icm.coansys.importers.models.DocumentDTO;
+
+public class RowComposer {
+	public static String composeRow(DocumentDTO docDTO) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(docDTO.getCollection());
+		if(docDTO.getMediaTypes().size()>0) sb.append(HBaseConstant.rowIdSeparator);
+		for(String t : docDTO.getMediaTypes()){
+			if(ProtoConstants.mediaTypePdf.equals(t)){
+				sb.append(HBaseConstant.rowIdMediaTypePdf);
+				sb.append(HBaseConstant.rowIdSeparator);
+			}
+		}
+		sb.append(HBaseConstant.rowIdSeparator);
+		sb.append(docDTO.getKey());
+		
+		return sb.toString();
+	}
+}
