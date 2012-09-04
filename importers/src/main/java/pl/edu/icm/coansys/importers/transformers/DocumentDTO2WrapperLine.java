75a54b1c802b37bdb7d56d0f3cdd1baa14f79229
Adam Kawa <kawa@icm.edu.pl>
2012-09-04 15:16:52 +0200

    Provide wrapper for document data (mproto, cproto, rowkey)

---
+++ importers/src/main/java/pl/edu/icm/coansys/importers/transformers/DocumentDTO2WrapperLine.java
@@ -0,0 +1,32 @@
+/*
+ * (C) 2010-2012 ICM UW. All rights reserved.
+ */
+
+
+package pl.edu.icm.coansys.importers.transformers;
+
+import com.google.protobuf.ByteString;
+
+import pl.edu.icm.coansys.importers.models.DocumentDTO;
+import pl.edu.icm.coansys.importers.models.DocumentProtosWrapper.DocumentWrapper;
+
+
+
+/**
+ * @author pdendek
+ */
+public class DocumentDTO2WrapperLine {
+	
+	public static String translate(DocumentDTO docDTO) {
+		
+		DocumentWrapper.Builder dw = DocumentWrapper.newBuilder();
+		
+		dw.setRowId(ByteString.copyFrom(RowComposer.composeRow(docDTO).getBytes()));
+		dw.setMproto(docDTO.getDocumentMetadata().toByteString());
+		dw.setCproto(docDTO.getMediaConteiner().toByteString());
+		
+		return dw.build().toByteString().toStringUtf8();
+	}
+        
+        
+}
