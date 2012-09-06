88c4f3d3494e15b3cd922ae41a0225481fda345c
Adam Kawa <kawa@icm.edu.pl>
2012-09-06 14:00:54 +0200

    Small improvements in Oozie Workflow

--- commons/src/main/java/pl/edu/icm/coansys/commons/pig/udf/DocumentProtobufBytesToTuple.java
+++ commons/src/main/java/pl/edu/icm/coansys/commons/pig/udf/DocumentProtobufBytesToTuple.java
@@ -4,6 +4,7 @@
  */
 package pl.edu.icm.coansys.commons.pig.udf;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +16,13 @@ import org.apache.pig.data.DataBag;
 import org.apache.pig.data.DataByteArray;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
+import org.apache.tika.exception.TikaException;
+import org.xml.sax.SAXException;
+import pl.edu.icm.coansys.commons.pdf.TikaPDFExtractor;
+import pl.edu.icm.coansys.importers.constants.ProtoConstants;
 import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentMetadata;
+import pl.edu.icm.coansys.importers.models.DocumentProtos.Media;
+import pl.edu.icm.coansys.importers.models.DocumentProtos.MediaContainer;
 
 /**
  *
@@ -39,20 +46,60 @@ public class DocumentProtobufBytesToTuple extends EvalFunc<Map> {
     @Override
     public Map exec(Tuple input) throws IOException {
         try {
-            DataByteArray protoMessage = (DataByteArray) input.get(0);
-
-            DocumentMetadata metadata = DocumentMetadata.parseFrom(protoMessage.get());
+            DataByteArray protoMetadata = (DataByteArray) input.get(0);
+            DocumentMetadata metadata = DocumentMetadata.parseFrom(protoMetadata.get());
 
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("key", metadata.getKey());
             map.put("title", metadata.getTitle());
-            map.put("keywordList", getBag(metadata.getKeywordList()));
+            map.put("keywords", getConcatenated(metadata.getKeywordList()));
+            map.put("abstract", metadata.getAbstrakt());
 
+            if (input.size() > 1) {
+                DataByteArray protoMedia = (DataByteArray) input.get(1);
+                if (protoMedia != null) {
+                MediaContainer media = MediaContainer.parseFrom(protoMedia.get());
+                String content = getFirstPDFContent(media.getMediaList());
+                if (content != null) {
+                    map.put("content", content);
+                }
+                }
+            }
+            
             return map;
-
         } catch (Exception e) {
             // Throwing an exception will cause the task to fail.
             throw new RuntimeException("Error while parsing DocumentMetadata", e);
         }
     }
+
+    private String getFirstPDFContent(List<Media> medias) throws IOException, SAXException, TikaException {
+        if (medias != null && medias.size() > 0) {
+            for (Media medium : medias) {
+                if (ProtoConstants.mediaTypePdf.equals(medium.getMediaType())) {
+                    ByteArrayInputStream inputStream = new ByteArrayInputStream(medium.getContent().toByteArray());
+                    try {
+                        String content = TikaPDFExtractor.getContent(inputStream);
+                        return content;
+                    } finally {
+                        inputStream.close();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+    private String getConcatenated(List<String> list) {
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+        String concatenated = list.get(0);
+        for (int i = 1; i < list.size(); ++i) {
+            concatenated += " " + list.get(i);
+        }
+        return concatenated;
+    }
+    
+  
 }
