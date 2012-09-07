8a79f8c622af77b09399e34429b43d7c2ac6ad91
Artur Czeczko <a.czeczko@icm.edu.pl>
2012-09-07 12:29:08 +0200

    - presentation of results

---
+++ logs-analysis/src/main/java/pl/edu/icm/coansys/logsanalysis/presentation/ReportMostPopular.java
@@ -0,0 +1,31 @@
+/*
+ * (C) 2010-2012 ICM UW. All rights reserved.
+ */
+package pl.edu.icm.coansys.logsanalysis.presentation;
+
+import java.io.IOException;
+import java.util.Date;
+import pl.edu.icm.coansys.logsanalysis.models.MostPopularProtos;
+import pl.edu.icm.coansys.logsanalysis.models.MostPopularProtos.ResourceStat;
+import pl.edu.icm.coansys.logsanalysis.transformers.BytesArray2SequenceFile;
+
+/**
+ *
+ * @author Artur Czeczko <a.czeczko@icm.edu.pl>
+ */
+public class ReportMostPopular {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.err.println("Usage: ReportMostPopular <file_uri>");
+            return;
+        }
+        for (byte[] bytes : BytesArray2SequenceFile.read(args[0])) {
+            MostPopularProtos.MostPopularStats stats = MostPopularProtos.MostPopularStats.parseFrom(bytes);
+            System.out.println("Most popular statistics from date " + new Date(stats.getTimestamp()));
+            for (ResourceStat rs : stats.getStatList()) {
+                System.out.println("Resource ID: " + rs.getResourceId() + "; usages: " + rs.getCounter());
+            }
+        }
+    }
+}
