1c80cd081b3aa9dc21a9609205ea7692c544caab
Adam Kawa <kawa@icm.edu.pl>
2012-09-24 10:49:08 +0200

    module that runs workflows made of code from other modules

---
+++ integration/src/main/oozie/submit-to-oozie.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TASK=$1
+USER=$2
+OOZIE_SERVER=$3
+PROPERTIES_FILE=$4
+
+WORKFLOW_HDFS_DIR="/user/${USER}/workflows/${TASK}"
+
+CD=`pwd`
+cd ../../../../document-similarity/src/main/oozie/
+./copy-to-oozie.sh similarity ${USER}
+cd ../../../../disambiguation-author/src/main/oozie/
+./copy-to-oozie.sh disambiguation-author-hdfs ${USER}
+cd ${CD}
+
+echo "Recreating workflow data in HDFS"
+sudo -u ${USER} hadoop fs -rm -r ${WORKFLOW_HDFS_DIR}
+sudo -u ${USER} hadoop fs -mkdir ${WORKFLOW_HDFS_DIR}
+echo "Putting current workflow data to HDFS"
+sudo -u ${USER} hadoop fs -put ${TASK}/* ${WORKFLOW_HDFS_DIR}
+echo "Submiting workflow to Oozzie Server: ${OOZIE_SERVER}:11000"
+sudo -u ${USER} oozie job -oozie http://${OOZIE_SERVER}:11000/oozie -config ${PROPERTIES_FILE} -run
