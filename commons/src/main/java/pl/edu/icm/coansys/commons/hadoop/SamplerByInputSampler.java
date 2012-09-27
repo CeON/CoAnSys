bbdce95f2f0f294c4c779ccd8ecaa9b2edb1bdb8
Adam Kawa <kawa@icm.edu.pl>
2012-09-27 14:25:48 +0200

    manual correcting from bad automatic Netbeans refactor

--- commons/src/main/java/pl/edu/icm/coansys/commons/hadoop/SamplerByInputSampler.java
+++ commons/src/main/java/pl/edu/icm/coansys/commons/hadoop/SamplerByInputSampler.java
@@ -1,20 +1,12 @@
 package pl.edu.icm.coansys.commons.hadoop;
 
-import pl.edu.icm.coansys.commons.hbase.*;
 import java.io.IOException;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.InputSampler;
 import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
 import org.apache.hadoop.util.GenericOptionsParser;
