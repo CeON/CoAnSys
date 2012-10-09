bf89cc1fab03af605e73ec30ed9fdb7e9a1110ac
Piotr Wendykier <p.wendykier@icm.edu.pl>
2012-10-09 15:14:22 +0200

    - Commented out unused field. - Organized imports.

--- document-classification/src/main/java/pl/edu/icm/coansys/classification/documents/jobs/WordCountReducer.java
+++ document-classification/src/main/java/pl/edu/icm/coansys/classification/documents/jobs/WordCountReducer.java
@@ -4,9 +4,7 @@
 package pl.edu.icm.coansys.classification.documents.jobs;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.Iterator;
-import java.util.Random;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.log4j.Level;
@@ -15,17 +13,16 @@ import pl.edu.icm.coansys.disambiguation.auxil.LoggingInDisambiguation;
 import pl.edu.icm.coansys.disambiguation.auxil.TextArrayWritable;
 
 /**
- * 
+ *
  * @author pdendek
  * @version 1.0
  * @since 2012-08-07
  */
 public class WordCountReducer extends Reducer<TextArrayWritable, IntWritable, TextArrayWritable, IntWritable> {
 
-	private static Logger logger = Logger.getLogger(LoggingInDisambiguation.class);
-	protected String reducerId = new Date().getTime() + "_" + new Random().nextFloat();
+    private static Logger logger = Logger.getLogger(LoggingInDisambiguation.class);
+    //protected String reducerId = new Date().getTime() + "_" + new Random().nextFloat();
 
-    
     @Override
     public void setup(Context context) throws IOException, InterruptedException {
         logger.setLevel(Level.DEBUG);
@@ -33,26 +30,27 @@ public class WordCountReducer extends Reducer<TextArrayWritable, IntWritable, Te
 
     @Override
     /**
-     * (IN) accepts key-value pairs containing K:docId + word from this document, V: value 1
-     * (PROCESS) count occurrences of the word
-     * (OUT) emits key-value pairs containing K:docId + word from this document, V: the number of word occurrences
+     * (IN) accepts key-value pairs containing K:docId + word from this
+     * document, V: value 1 (PROCESS) count occurrences of the word (OUT) emits
+     * key-value pairs containing K:docId + word from this document, V: the
+     * number of word occurrences
      */
     public void reduce(TextArrayWritable key, Iterable<IntWritable> values, Context context) {
-    	
-    	int wc = 0;
+
+        int wc = 0;
         for (Iterator it = values.iterator(); it.hasNext();) {
             wc++;
-            
+
         }
-    	
-    	try {
-			context.write(key, new IntWritable(wc));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+        try {
+            context.write(key, new IntWritable(wc));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
