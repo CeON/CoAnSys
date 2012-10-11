531daff2599b593da5eae726cfd156a9f145421d
Piotr Dendek <pdendek@icm.edu.pl>
2012-10-11 11:02:40 +0000

    apache pig UDFs for extracting data from our protocol buffers

---
+++ document-classification/src/main/java/pl/edu/icm/coansys/classification/documents/pig/extractors/EXTRACT_MAP_WHEN_CATEG_LIM.java
@@ -0,0 +1,81 @@
+package pl.edu.icm.coansys.classification.documents.pig.extractors;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.DataByteArray;
+import org.apache.pig.data.DataType;
+import org.apache.pig.data.DefaultDataBag;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.logicalLayer.FrontendException;
+import org.apache.pig.impl.logicalLayer.schema.Schema;
+
+import pl.edu.icm.coansys.importers.models.DocumentProtos.ClassifCode;
+import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentMetadata;
+
+/**
+*
+* @author akawa
+*/
+@SuppressWarnings("rawtypes")
+public class EXTRACT_MAP_WHEN_CATEG_LIM extends EvalFunc<Map> {
+
+	@Override
+	public Schema outputSchema(Schema p_input){
+		try{
+			return Schema.generateNestedSchema(DataType.MAP);
+		}catch(FrontendException e){
+			throw new IllegalStateException(e);
+		}
+	}	
+	
+   @Override
+   public Map exec(Tuple input) throws IOException {
+       try {
+           DataByteArray protoMetadata = (DataByteArray) input.get(0);
+           DocumentMetadata metadata = DocumentMetadata.parseFrom(protoMetadata.get());
+           Integer lim = Integer.parseInt((String) input.get(1));
+           DataBag db = getCategories(metadata.getClassifCodeList());
+           if(db.size() >= lim){
+        	   Map<String, Object> map = new HashMap<String, Object>();
+               map.put("key", metadata.getKey());
+               map.put("title", metadata.getTitle());
+               map.put("keywords", getConcatenated(metadata.getKeywordList()));
+               map.put("abstract", metadata.getAbstrakt());
+               map.put("categories", db);
+               return map;
+           }
+           return null;
+           
+       } catch (Exception e) {
+           // Throwing an exception will cause the task to fail.
+           throw new RuntimeException("Error while parsing DocumentMetadata", e);
+       }
+   }
+
+   private DataBag getCategories(List<ClassifCode> classifCodeList) {
+	   DataBag db = new DefaultDataBag();
+	   for(ClassifCode code : classifCodeList)
+       	for(String co_str : code.getValueList()){
+//       		System.out.print(" "+co_str);
+       		db.add(TupleFactory.getInstance().newTuple(co_str));
+       	}
+	return db;
+}
+
+private String getConcatenated(List<String> list) {
+       if (list == null || list.isEmpty()) {
+           return null;
+       }
+       String concatenated = list.get(0);
+       for (int i = 1; i < list.size(); ++i) {
+           concatenated += " " + list.get(i);
+       }
+       return concatenated;
+   }
+}
---
+++ document-classification/src/main/java/pl/edu/icm/coansys/classification/documents/pig/extractors/REPLACE_FIELD.java
@@ -0,0 +1,35 @@
+package pl.edu.icm.coansys.classification.documents.pig.extractors;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.Tuple;
+
+/**
+*
+* @author akawa
+*/
+@SuppressWarnings("rawtypes")
+public class REPLACE_FIELD extends EvalFunc<Map> {
+
+   
+@Override
+   public Map exec(Tuple input) throws IOException {
+       try {
+    	   @SuppressWarnings("unchecked")
+    	   Map<String,Object> map = (Map<String,Object>) input.get(0);
+    	   DataBag db = (DataBag) input.get(1);
+    	   String fieldName = (String) input.get(2);
+    	   
+    	   map.put(fieldName, db);
+    	   
+           return map;
+           
+       } catch (Exception e) {
+           // Throwing an exception will cause the task to fail.
+           throw new RuntimeException("Error while parsing DocumentMetadata", e);
+       }
+   }
+}
