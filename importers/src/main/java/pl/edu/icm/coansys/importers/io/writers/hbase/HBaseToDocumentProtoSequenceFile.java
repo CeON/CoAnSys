d79c2cb61e5eb740b28514f85baeb0867b8fb426
Adam Kawa <kawa@icm.edu.pl>
2012-10-11 15:06:00 +0200

    counter to count skipped dprotos

--- importers/src/main/java/pl/edu/icm/coansys/importers/io/writers/hbase/HBaseToDocumentProtoSequenceFile.java
+++ importers/src/main/java/pl/edu/icm/coansys/importers/io/writers/hbase/HBaseToDocumentProtoSequenceFile.java
@@ -62,7 +62,7 @@ public class HBaseToDocumentProtoSequenceFile implements Tool {
         private ResultToProtoBytesConverter converter = new ResultToProtoBytesConverter();
         private DocumentWrapper.Builder dw = DocumentWrapper.newBuilder();
         private MultipleOutputs mos = null;
-        private int MAX_SIZE = 10000000;
+        private int MAX_SIZE = 5000000;
 
         @Override
         public void setup(Context context) {
