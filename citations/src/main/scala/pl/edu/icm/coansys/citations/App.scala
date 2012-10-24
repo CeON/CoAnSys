a57ed7ae06dfebfa85fd2cf21e388fcd5f0319d8
Mateusz Fedoryszak <matfed@icm.edu.pl>
2012-10-24 17:57:54 +0200

    Working indexing

--- citations/src/main/scala/pl/edu/icm/coansys/citations/App.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/App.scala
@@ -4,7 +4,7 @@ import java.net.URI
 import com.nicta.scoobi.application.ScoobiApp
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{Path, FileSystem}
-import org.apache.hadoop.io.{SequenceFile, MapFile, Writable}
+import org.apache.hadoop.io._
 
 import pl.edu.icm.coansys.commons.scala.strings.rotations
 import pl.edu.icm.coansys.commons.scala.automatic_resource_management.using
@@ -14,6 +14,7 @@ import com.nicta.scoobi.InputsOutputs.{convertToSequenceFile, toSequenceFile}
 import java.io.{DataInput, DataOutput}
 import com.nicta.scoobi.io.sequence.SeqSchema
 import collection.mutable.ListBuffer
+import org.apache.hadoop.io.SequenceFile.Sorter
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -21,21 +22,39 @@ import collection.mutable.ListBuffer
 object App extends ScoobiApp {
   override def upload = false
 
+  def extractSeqTypes(uri: String): (Class[_], Class[_]) = {
+    val conf = new Configuration()
+    val fs = FileSystem.get(URI.create(uri), conf)
+    val path = new Path(uri)
+    using(new SequenceFile.Reader(fs, path, conf)) {
+      reader =>
+        val keyClass = reader.getKeyClass
+        val valueClass = reader.getValueClass
+        (keyClass, valueClass)
+    }
+  }
+
   def convertSeqToMap(uri: String) {
     val conf = new Configuration()
     val fs = FileSystem.get(URI.create(uri), conf)
     val map = new Path(uri)
     val mapData = new Path(map, MapFile.DATA_FILE_NAME)
-    using(new SequenceFile.Reader(fs, map, conf)) {
-      reader =>
-        val keyClass = reader.getKeyClass
-        val valueClass = reader.getValueClass
-        MapFile.fix(fs, map, keyClass.asInstanceOf[Class[_ <: Writable]], valueClass.asInstanceOf[Class[_ <: Writable]], false, conf)
-    }
+    val (keyClass, valueClass) = extractSeqTypes(mapData.toUri.toString)
+    MapFile.fix(fs, map, keyClass.asInstanceOf[Class[_ <: Writable]], valueClass.asInstanceOf[Class[_ <: Writable]], false, conf)
+  }
+
+  def mergeSeqs(uri: String) {
+    val conf = new Configuration()
+    val fs = FileSystem.get(URI.create(uri), conf)
+    val dir = new Path(uri)
+    val paths: Array[Path] = fs.listStatus(dir).map(_.getPath)
+    val mapData = new Path(dir, MapFile.DATA_FILE_NAME)
+    val (keyClass, valueClass) = extractSeqTypes(paths(0).toUri.toString)
+    val sorter = new Sorter(fs, keyClass.asInstanceOf[Class[_ <: WritableComparable[_]]], valueClass, conf)
+    sorter.sort(paths, mapData, true)
   }
 
   def buildIndex(readDocs: () => DList[MockDocumentWrapper], indexFile: String) {
-    //    def readDocs(): TypedPipe[MockDocumentWrapper] = throw new RuntimeException
     def indexEntries(allDocs: DList[MockDocumentWrapper]) = {
       val tokensWithDocs =
         allDocs
@@ -48,8 +67,17 @@ object App extends ScoobiApp {
 
       rotationsWithDocs
     }
-    implicit val docsIterableSchema = new IterableSchema(new MockDocumentWrapper())
+    implicit object docsIterableSchema extends SeqSchema[Iterable[MockDocumentWrapper]] {
+      def toWritable(x: Iterable[MockDocumentWrapper]) = new MockDocumentWritableIterable(x)
+
+      def fromWritable(x: SeqType) =
+        x.iterable
+
+      type SeqType = MockDocumentWritableIterable
+      val mf = manifest[MockDocumentWritableIterable]
+    }
     persist(convertToSequenceFile(indexEntries(readDocs()), indexFile))
+    mergeSeqs(indexFile)
     convertSeqToMap(indexFile)
   }
 
