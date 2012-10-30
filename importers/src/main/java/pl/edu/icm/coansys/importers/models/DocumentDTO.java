70aa05af1648703e94982da6e1770304dbef014e
Adam Kawa <kawa@icm.edu.pl>
2012-10-30 17:39:25 +0100

    effect of changes of .proto file

--- importers/src/main/java/pl/edu/icm/coansys/importers/models/DocumentDTO.java
+++ importers/src/main/java/pl/edu/icm/coansys/importers/models/DocumentDTO.java
@@ -1,8 +1,6 @@
 /*
  * (C) 2010-2012 ICM UW. All rights reserved.
  */
-
-
 package pl.edu.icm.coansys.importers.models;
 
 import java.util.ArrayList;
@@ -17,67 +15,89 @@ import pl.edu.icm.coansys.importers.models.DocumentProtos.MediaContainer;
  */
 public class DocumentDTO {
 
-	private String collection;
+    private String collection;
 //	protected String year;
-	private DocumentMetadata docMetadata;
-	private String key;
-	private MediaContainer.Builder medias = MediaContainer.newBuilder(); 
-	private ArrayList<String> mediaTypes = new ArrayList<String>();
+    private DocumentMetadata docMetadata;
+    private String key;
+    private MediaContainer.Builder medias = MediaContainer.newBuilder();
+    private ArrayList<String> mediaTypes = new ArrayList<String>();
+    private String sourcePath;
+    private String archiveZip;
 
-	public String getCollection() {
-		return collection;
-	}
+    public String getCollection() {
+        return collection;
+    }
 
-	public void setCollection(String collection) {
-		this.collection = collection;
-	}
+    public void setCollection(String collection) {
+        this.collection = collection;
+    }
 
-//	public String getYear() {
-//		return year;
-//	}
-//
-//	public void setYear(String year) {
-//		this.year = year;
-//	}
-	
-	public DocumentDTO setDocumentMetadata(DocumentMetadata docMetadata) {
-		this.docMetadata = docMetadata;
-		return this;
-	}
+    public DocumentDTO setDocumentMetadata(DocumentMetadata docMetadata) {
+        this.docMetadata = docMetadata;
+        return this;
+    }
 
-	public DocumentMetadata getDocumentMetadata() {
-		return docMetadata;
-	}
-	
-	public DocumentDTO setKey(String key) {
-		this.key=key;
-		return this;
-	}
+    public DocumentMetadata getDocumentMetadata() {
+        return docMetadata;
+    }
 
-	public String getKey() {
-		return key;
-	}
-	
-	public DocumentDTO addMedia(Media media){
-		this.medias.addMedia(media);
-		return this;
-	}
-	
-	public DocumentDTO addMedia(Collection medias){
-		this.medias.addAllMedia(medias);
-		return this;
-	}
+    public DocumentDTO setKey(String key) {
+        this.key = key;
+        return this;
+    }
 
-	public MediaContainer getMediaConteiner(){
-		return medias.build();
-	}
-	
-	public DocumentDTO addMediaType(String type) {
-		mediaTypes.add(type);
-		return this;
-	}
-	
-	public List<String> getMediaTypes(){
-		return this.mediaTypes;
-	}
+    public String getKey() {
+        return key;
+    }
+
+    public DocumentDTO addMedia(Media media) {
+        this.medias.addMedia(media);
+        return this;
+    }
+
+    public DocumentDTO addMedia(Collection medias) {
+        this.medias.addAllMedia(medias);
+        return this;
+    }
+
+    public MediaContainer getMediaConteiner() {
+        return medias.build();
+    }
+
+    public DocumentDTO addMediaType(String type) {
+        mediaTypes.add(type);
+        return this;
+    }
+
+    public List<String> getMediaTypes() {
+        return this.mediaTypes;
+    }
+
+    /**
+     * @return the sourcePath
+     */
+    public String getSourcePath() {
+        return sourcePath;
+    }
+
+    /**
+     * @param sourcePath the sourcePath to set
+     */
+    public void setSourcePath(String sourcePath) {
+        this.sourcePath = sourcePath;
+    }
+
+    /**
+     * @return the archiveZip
+     */
+    public String getArchiveZip() {
+        return archiveZip;
+    }
+
+    /**
+     * @param archiveZip the archiveZip to set
+     */
+    public void setArchiveZip(String archiveZip) {
+        this.archiveZip = archiveZip;
+    }
 }
