d1edc87ea5e4741c10bf715306d68d2aa74ef820
Adam Kawa <akawa@hadoop.vls.icm.edu.pl>
2012-10-30 17:14:11 +0000

    making first step as simple as possible

--- importers/src/main/bash/import-via-sequence-files.sh
+++ importers/src/main/bash/import-via-sequence-files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# example:
+# ./import-via-sequence-files.sh sf ../../../target/importers-1.0-SNAPSHOT-jar-with-dependencies.jar /mnt/tmp/bwndata/bazekon-20120228 bazekon /mnt/tmp/bazekon.sf bazekon.sh false null
+
 MODE=$1
 IMPORTERS_JAR=$2
 COLLECTION_ZIPS_DIR=$3
@@ -8,18 +12,17 @@ COLLECTION_SEQUENCE_FILE_HDFS_DIR=$6
 BULK_HFILE=$7
 HBASE_TABLENAME=$8
 
-echo "truncate '${HBASE_TABLENAME}'" | hbase shell
-
 # create sequence file and move it to HDFS (single java application)
 if [ "${MODE}" = "sf" ] || [ "${MODE}" = "all" ]; then
 	rm -rf ${COLLECTION_SEQUENCE_FILE}
 	java -cp ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.file.BwmetaToDocumentWraperSequenceFileWriter ${COLLECTION_ZIPS_DIR} ${COLLECTION_NAME} ${COLLECTION_SEQUENCE_FILE}
-	hadoop fs -rm -r ${COLLECTION_SEQUENCE_FILE_HDFS_DIR}
-	hadoop fs -moveFromLocal ${COLLECTION_SEQUENCE_FILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR}
 fi
 
 # create hfile (mapreduce job)
 if [ "${MODE}" = "hfile" ] || [ "${MODE}" = "all" ]; then
+	echo "truncate '${HBASE_TABLENAME}'" | hbase shell
+	hadoop fs -rm -r ${COLLECTION_SEQUENCE_FILE_HDFS_DIR}
+	hadoop fs -moveFromLocal ${COLLECTION_SEQUENCE_FILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR}
 	hadoop fs -rm -r ${BULK_HFILE}
 	hadoop jar ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.hbase.DocumentWrapperSequenceFileToHBase -Dbulk.output=${BULK_HFILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} ${HBASE_TABLENAME}
 	hadoop fs -chmod -R 777 ${BULK_HFILE}
