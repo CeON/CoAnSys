bbcecf7748ab6f953d51fb1d1ee00d67437298b2
Adam Kawa <kawa@icm.edu.pl>
2012-10-31 21:30:26 +0100

    prespliting table on regions based on input to be loaded

---
+++ importers/src/main/bash/create-presplited-table.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# command-line paramters
+LOCAL_INPUT_PATH=$1
+HDFS_INPUT_PATH=$2
+OUTPUT_PATH=$3
+TABLE_NAME=$4
+TABLE_COLFAMS=$5
+TABLE_REGION_CNT=$6
+
+# global parameters
+COMMONS_JAR=../../../../commons/target/commons-1.0-SNAPSHOT.jar
+HDFS_KEYS_FILE=keys
+
+export HBASE_CLASSPATH=${COMMONS_JAR}
+
+hadoop fs -rm -r ${OUTPUT_PATH}
+hadoop fs -rm -r ${HDFS_INPUT_PATH}
+hadoop fs -mkdir ${HDFS_INPUT_PATH}
+hadoop fs -put ${LOCAL_INPUT_PATH} ${HDFS_INPUT_PATH}
+
+time hadoop jar ${COMMONS_JAR} pl.edu.icm.coansys.commons.hbase.SequenceFileKeysSamplerMR -libjars ${COMMONS_JAR} -D sampler.samples.region.count=${TABLE_REGION_CNT} ${HDFS_INPUT_PATH} ${OUTPUT_PATH}
+
+hadoop fs -rm ${HDFS_KEYS_FILE}
+hadoop fs -mv ${OUTPUT_PATH}/part-r-00000 ${HDFS_KEYS_FILE}
+
+time hbase pl.edu.icm.coansys.commons.hbase.HBaseTableUtils false DROP ${TABLE_NAME}
+time hbase org.apache.hadoop.hbase.util.RegionSplitter -D split.algorithm=pl.edu.icm.coansys.commons.hbase.SequenceFileSplitAlgorithm -f ${TABLE_COLFAMS} -c ${TABLE_REGION_CNT} ${TABLE_NAME}
