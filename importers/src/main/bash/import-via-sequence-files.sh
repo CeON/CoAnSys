4343980c28db808e080119eff6e3f9466b9e7d88
Adam Kawa <akawa@hadoop.vls.icm.edu.pl>
2012-10-31 15:57:41 +0000

    remove log

--- importers/src/main/bash/import-via-sequence-files.sh
+++ importers/src/main/bash/import-via-sequence-files.sh
@@ -13,11 +13,14 @@ COLLECTION_SEQUENCE_FILE=$5
 COLLECTION_SEQUENCE_FILE_HDFS_DIR=$6
 BULK_HFILE=$7
 HBASE_TABLENAME=$8
+LOG_FILE=report.log
+
 
 # create sequence file and move it to HDFS (single java application)
 if [ "${MODE}" = "sf" ] || [ "${MODE}" = "all" ]; then
+	rm -rf ${LOG_FILE}
 	rm -rf ${COLLECTION_SEQUENCE_FILE}
-	java -cp ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.file.BwmetaToDocumentWraperSequenceFileWriter ${COLLECTION_ZIPS_DIR} ${COLLECTION_NAME} ${COLLECTION_SEQUENCE_FILE}
+	java -cp ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.file.BwmetaToDocumentWraperSequenceFileWriter ${COLLECTION_ZIPS_DIR} ${COLLECTION_NAME} ${COLLECTION_SEQUENCE_FILE} log4j.properties
 fi
 
 # create hfile (mapreduce job)
