66320bc0037ec25354d2451ae3d258d37f135590
Adam Kawa <kawa@icm.edu.pl>
2012-10-31 21:30:54 +0100

    simple script that contains more custom ones

---
+++ importers/src/main/bash/run-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# command-line parameters
+LOCAL_BWNDATA_SEQUENCE_FILE=$1
+TABLE_NAME=$2
+
+# default parameters
+COLLECTION_SEQUENCE_FILE_HDFS_DIR=/user/akawa/${TABLE_NAME}_sf
+TABLE_HFILES_HDFS_DIR=${TABLE_NAME}_hfiles
+TABLE_REGION_CNT=20
+
+time ./create-presplited-table.sh ${LOCAL_BWNDATA_SEQUENCE_FILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} hbasekeys presplited m:c ${TABLE_REGION_CNT}
+time ./bulkload-sequence-file.sh all ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} ${TABLE_HFILES_HDFS_DIR} ${TABLE_NAME}
