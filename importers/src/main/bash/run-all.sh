80771eb9a7451d720ac9e586599008c5224f4835
Adam Kawa <akawa@hadoop.vls.icm.edu.pl>
2012-11-01 22:32:06 +0000

    adding parameter moving file(s) to hdfs or not

--- importers/src/main/bash/run-all.sh
+++ importers/src/main/bash/run-all.sh
@@ -9,5 +9,5 @@ COLLECTION_SEQUENCE_FILE_HDFS_DIR=/user/akawa/${TABLE_NAME}_sf
 TABLE_HFILES_HDFS_DIR=${TABLE_NAME}_hfiles
 TABLE_REGION_CNT=20
 
-time ./create-presplited-table.sh ${LOCAL_BWNDATA_SEQUENCE_FILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} hbasekeys presplited m:c ${TABLE_REGION_CNT} false
+time ./create-presplited-table.sh ${LOCAL_BWNDATA_SEQUENCE_FILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} hbasekeys presplited m:c ${TABLE_REGION_CNT} true
 time ./bulkload-sequence-file.sh all ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} ${TABLE_HFILES_HDFS_DIR} ${TABLE_NAME}
