7d3c37da7ee9b63fec50a4fc23db496bb6351467
Adam Kawa <akawa@hadoop.vls.icm.edu.pl>
2012-11-09 14:15:42 +0000

    adding zookeeper information, hbase input datable to workflow

---
+++ document-classification/src/main/oozie/copy-to-oozie.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TASK=$1
+USER=$2
+RM=$3
+
+if [ `hostname` = "hadoop" ]; then
+	WORKFLOW_HDFS_DIR="/user/${USER}/workflows/${TASK}"
+else
+	WORKFLOW_HDFS_DIR="hdfs://localhost.localdomain:8020/user/${USER}/workflows/${TASK}"
+fi
+
+WORKFLOW_LOCAL_LIB_DIR=${TASK}/workflow/lib
+WORKFLOW_LOCAL_PIG_DIR=${TASK}/workflow/pig
+
+echo "Removing ${WORKFLOW_LOCAL_LIB_DIR} and ${WORKFLOW_LOCAL_PIG_DIR}"
+if [ "${RM}" = "RM" ]; then
+	rm -r -f ${WORKFLOW_LOCAL_LIB_DIR}
+	rm -r -f ${WORKFLOW_LOCAL_PIG_DIR}
+fi
+
+if [ ! -d "$WORKFLOW_LOCAL_LIB_DIR" ]; then
+    mkdir ${WORKFLOW_LOCAL_LIB_DIR}
+fi
+
+if [ ! -d "$WORKFLOW_LOCAL_PIG_DIR" ]; then
+    mkdir ${WORKFLOW_LOCAL_PIG_DIR}
+fi
+
+echo "Copying required libaries to ${TASK}/lib"
+cp /usr/lib/pig/pig-0.9.2-cdh4.0.1.jar ${WORKFLOW_LOCAL_LIB_DIR}
+cp ../../../../document-classification/target/document-classification-1.0-SNAPSHOT.jar ${WORKFLOW_LOCAL_LIB_DIR}
+cp ../../../../document-classification/target/document-classification-1.0-SNAPSHOT-only-dependencies.jar ${WORKFLOW_LOCAL_LIB_DIR}
+
+echo "Copying required pig scripts to ${TASK}"
+cp -r ../pig/*.pig ${WORKFLOW_LOCAL_PIG_DIR}
+
+echo "Recreating workflow data in HDFS"
+hadoop fs -rm -r ${WORKFLOW_HDFS_DIR}
+hadoop fs -mkdir ${WORKFLOW_HDFS_DIR}
+echo "Putting current workflow data to HDFS"
+hadoop fs -put ${TASK}/* ${WORKFLOW_HDFS_DIR}
