e5f4d17bd8208768d52013d207da00677f502fde
Adam Kawa <kawa@icm.edu.pl>
2012-11-14 17:42:07 +0100

    changing String types to byte[]

--- document-similarity/src/main/java/pl/edu/icm/coansys/similarity/documents/function/AvgSimilarityFunction.java
+++ document-similarity/src/main/java/pl/edu/icm/coansys/similarity/documents/function/AvgSimilarityFunction.java
@@ -2,12 +2,6 @@ package pl.edu.icm.coansys.similarity.documents.function;
 
 import java.util.List;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
  *
  * @author akawa
@@ -17,7 +11,7 @@ public class AvgSimilarityFunction implements SimilarityFunction {
     private static final double EMPTY_VALUE = 0d;
 
     @Override
-    public double getDocumentsKeywordSimilarity(String keyword, byte[] doc1key, double doc1keywordWeight, byte[] doc2key, double doc2keywordWeight) {
+    public double getDocumentsKeywordSimilarity(byte[] keyword, byte[] doc1key, double doc1keywordWeight, byte[] doc2key, double doc2keywordWeight) {
         return doc1keywordWeight * doc2keywordWeight;
     }
 
--- document-similarity/src/main/java/pl/edu/icm/coansys/similarity/documents/function/SimilarityFunction.java
+++ document-similarity/src/main/java/pl/edu/icm/coansys/similarity/documents/function/SimilarityFunction.java
@@ -7,6 +7,6 @@ import java.util.List;
  * @author akawa
  */
 public interface SimilarityFunction {
-    double getDocumentsKeywordSimilarity(String keyword, byte[] doc1key, double doc1keywordWeight, byte[] doc2key, double doc2keywordWeight);
+    double getDocumentsKeywordSimilarity(byte[] keyword, byte[] doc1key, double doc1keywordWeight, byte[] doc2key, double doc2keywordWeight);
     double getDocumentsTotalSimilarity(byte[] doc1key, byte[] doc2key, List<Double> keywordsSimilarities);
 }
