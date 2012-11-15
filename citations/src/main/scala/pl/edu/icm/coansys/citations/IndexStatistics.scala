c14ff81481eee1db5db4d15e4babd64e0611f2d5
Mateusz Fedoryszak <matfed@icm.edu.pl>
2012-11-15 10:29:55 +0100

    index statistics computer

---
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/IndexStatistics.scala
@@ -0,0 +1,31 @@
+package pl.edu.icm.coansys.citations
+
+import com.nicta.scoobi.application.ScoobiApp
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{Text, MapFile}
+
+/**
+ * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+ */
+object IndexStatistics extends ScoobiApp {
+  override def upload = false
+
+  def run() {
+    val indexFileUri = args(0)
+    val reader = new MapFile.Reader(new Path(indexFileUri), configuration)
+    val key = new Text()
+    val value = new BytesIterable()
+    println("Entries:")
+    var noKey = 0
+    var noVal = 0
+    while (reader.next(key, value)) {
+      print(key.toString)
+      print("\t")
+      println(value.iterable.size)
+      noKey += 1
+      noVal += value.iterable.size
+    }
+    println("All entries: " + noKey)
+    println("All values: " + noVal)
+  }
+}
