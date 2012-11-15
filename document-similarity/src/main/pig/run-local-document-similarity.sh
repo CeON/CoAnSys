497351dedf35bdf0a1fc32f9199e4f707b89266b
Adam Kawa <kawa@icm.edu.pl>
2012-11-15 14:58:34 +0100

    script to run document-similarity on my personal laptop ;)

--- document-similarity/src/main/pig/run-local-document-similarity.sh
+++ document-similarity/src/main/pig/run-local-document-similarity.sh
@@ -4,4 +4,4 @@ OUTPUT_PATH=$1
 JARS=../../../../integration/src/main/oozie/full/workflow/lib/*.jar
 
 hadoop fs -rmr ${OUTPUT_PATH}
-pig -p commonJarsPath=${JARS} -p parallel=1 -p inputPath=full/hbase-dump/mproto-m* -p outputPath=${OUTPUT_PATH} -p mapredChildJavaOpts=-Xmx2000m document-similarity.pig
+pig -p similarityTopnDocumentPerDocument=2 -p commonJarsPath=${JARS} -p parallel=2 -p inputPath=full/hbase-dump/mproto-m* -p outputPath=${OUTPUT_PATH} -p mapredChildJavaOpts=-Xmx2000m document-similarity.pig
