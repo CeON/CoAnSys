7287ff8163a396d0906901fdfda9de9a14063bda
Adam Kawa <akawa@hadoop.vls.icm.edu.pl>
2012-11-26 11:12:25 +0000

    refactor

--- importers/src/main/bash/bulkload-sequence-file.sh
+++ importers/src/main/bash/bulkload-sequence-file.sh
@@ -3,24 +3,21 @@
 # command-line parameters
 MODE=$1
 IMPORTERS_JAR=$2
-COLLECTION_SEQUENCE_FILE_HDFS_DIR=$3
-BULK_HFILE=$4
+HDFS_SEQUENCE_FILE_INPUT_DIR=$3
+HDFS_BULK_HFILE_OUTPUT_DIR=$4
 HBASE_TABLENAME=$5
 
 # create hfile (mapreduce job)
 if [ "${MODE}" = "hfile" ] || [ "${MODE}" = "all" ]; then
         echo "truncate '${HBASE_TABLENAME}'" | hbase shell
-        hadoop fs -rm -r ${COLLECTION_SEQUENCE_FILE_HDFS_DIR}
-        hadoop fs -rm -r ${BULK_HFILE}
-        hadoop jar ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.hbase.DocumentWrapperSequenceFileToHBase -Dbulk.output=${BULK_HFILE} ${COLLECTION_SEQUENCE_FILE_HDFS_DIR} ${HBASE_TABLENAME}
-        hadoop fs -chmod -R 777 ${BULK_HFILE}
+        hadoop fs -rm -r ${HDFS_SEQUENCE_FILE_INPUT_DIR}
+        hadoop fs -rm -r ${HDFS_BULK_HFILE_OUTPUT_DIR}
+        hadoop jar ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.hbase.DocumentWrapperSequenceFileToHBase -Dbulk.output=${HDFS_BULK_HFILE_OUTPUT_DIR} ${HDFS_SEQUENCE_FILE_INPUT_DIR} ${HBASE_TABLENAME}
+        hadoop fs -chmod -R 777 ${HDFS_BULK_HFILE_OUTPUT_DIR}
 fi
 
 # complete bulk loading
 if [ "${MODE}" = "bulk" ] || [ "${MODE}" = "all" ]; then
-        hadoop jar /usr/lib/hbase/hbase-0.92.1-cdh4.0.1-security.jar completebulkload ${BULK_HFILE} ${HBASE_TABLENAME}
-        # clean temporary directories
-        hadoop fs -rm -r ${COLLECTION_SEQUENCE_FILE_HDFS_DIR}
-        hadoop fs -rm -r ${BULK_HFILE}
+        hadoop jar /usr/lib/hbase/hbase-0.92.1-cdh4.0.1-security.jar completebulkload ${HDFS_BULK_HFILE_OUTPUT_DIR} ${HBASE_TABLENAME}
 fi
 
--- importers/src/main/bash/create-presplited-table.sh
+++ importers/src/main/bash/create-presplited-table.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# Create HBase table presplited by regions based on sampling dataset to be imported
+
 # required command-line paramters
-HDFS_INPUT_PATH=$1
-SAMPLES_OUTPUT_PATH=$2
-TABLE_NAME=$3
-TABLE_COLFAMS=$4
-TABLE_REGION_CNT=$5
+IMPORTERS_JAR=$1
+HDFS_INPUT_PATH=$2
+SAMPLES_OUTPUT_PATH=$3
+TABLE_NAME=$4
+TABLE_COLFAMS=$5
+TABLE_REGION_CNT=$6
 
 # optional parameters
-LOAD_INPUT_TO_HDFS=$6
-LOCAL_INPUT_PATH=$7
+LOAD_INPUT_TO_HDFS=$7
+LOCAL_INPUT_PATH=$8
 
 # global parameters
-COMMONS_JAR=../../../../commons/target/commons-1.0-SNAPSHOT.jar
 HDFS_KEYS_FILE=keys
 
-export HBASE_CLASSPATH=${COMMONS_JAR}
+export HBASE_CLASSPATH=${IMPORTERS_JAR}
 
 if [ "${LOAD_INPUT_TO_HDFS}" = "true" ]; then
 	hadoop fs -rm -r ${HDFS_INPUT_PATH}
@@ -25,10 +27,11 @@ fi
 
 hadoop fs -rm -r ${SAMPLES_OUTPUT_PATH}
 
-time hadoop jar ${COMMONS_JAR} pl.edu.icm.coansys.commons.hbase.SequenceFileKeysSamplerMR -libjars ${COMMONS_JAR} -D sampler.samples.region.count=${TABLE_REGION_CNT} ${HDFS_INPUT_PATH} ${SAMPLES_OUTPUT_PATH}
+hadoop jar ${IMPORTERS_JAR} pl.edu.icm.coansys.commons.hbase.SequenceFileKeysSamplerMR -libjars ${IMPORTERS_JAR} -D sampler.samples.region.count=${TABLE_REGION_CNT} ${HDFS_INPUT_PATH} ${SAMPLES_OUTPUT_PATH}
 
 hadoop fs -rm ${HDFS_KEYS_FILE}
 hadoop fs -mv ${SAMPLES_OUTPUT_PATH}/part-r-00000 ${HDFS_KEYS_FILE}
 
-time hbase pl.edu.icm.coansys.commons.hbase.HBaseTableUtils false DROP ${TABLE_NAME}
-time hbase org.apache.hadoop.hbase.util.RegionSplitter -D split.algorithm=pl.edu.icm.coansys.commons.hbase.SequenceFileSplitAlgorithm -f ${TABLE_COLFAMS} -c ${TABLE_REGION_CNT} ${TABLE_NAME}
+# potentially drop and re-create table
+hbase pl.edu.icm.coansys.commons.hbase.HBaseTableUtils false DROP ${TABLE_NAME}
+hbase org.apache.hadoop.hbase.util.RegionSplitter -D split.algorithm=pl.edu.icm.coansys.commons.hbase.SequenceFileSplitAlgorithm -f ${TABLE_COLFAMS} -c ${TABLE_REGION_CNT} ${TABLE_NAME}
