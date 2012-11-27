cbbbf9b63eab27ddf6f14d49bbc8b5e2ad063ca0
Adam Kawa <kawa@icm.edu.pl>
2012-11-27 15:37:43 +0100

    runs document-similarity on cluster

---
+++ document-similarity/src/main/pig/run-cluster-docsim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_TFIDF=document-tfidf-p1.pig
+SCRIPT_DOCSIM=document-similarity-p2.pig
+OUTPUT_PATH=document-similarity
+JARS=../../../../integration/src/main/oozie/full/workflow/lib/*.jar
+
+TFIDF_INPUT_PATH="${OUTPUT_PATH}/tfidf/all-topn"
+PARALLEL=20
+
+hadoop fs -rm -r ${OUTPUT_PATH}
+time pig -p tmpCompressionCodec=lzo -p tfidfMinValue=0.25 -p sample=0.1 -p commonJarsPath=${JARS} -p parallel=${PARALLEL} -p inputPath=full/hbase-dump/mproto-m* -p outputPath=${OUTPUT_PATH} -p mapredChildJavaOpts=-Xmx10000m -f ${SCRIPT_TFIDF}
+time pig -p tmpCompressionCodec=lzo -p commonJarsPath=${JARS} -p similarityTopnDocumentPerDocument=20 -p parallel=${PARALLEL} -p inputPath=${TFIDF_INPUT_PATH} -p outputPath=${OUTPUT_PATH} -p mapredChildJavaOpts=-Xmx10000m -f ${SCRIPT_DOCSIM}
+
