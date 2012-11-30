c674d9d2c347957376bb759557a75194575a2be2
Adam Kawa <akawa@hadoop.vls.icm.edu.pl>
2012-11-30 09:09:27 +0000

    refactor

--- importers/src/main/bash/bulkload-sequence-file.sh
+++ importers/src/main/bash/bulkload-sequence-file.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-# command-line parameters
-MODE=$1
-IMPORTERS_JAR=$2
+# ------------------------------------------------------------------
+Loads the data from Sequence Files into HBase table as MR map-only job.
+The mapper class can be passed as parameter. 
+Load can be done in two ways:
+1) using PUT API
+
+To use PUT API,
+MODE should be "hfileput"
+HDFS_BULK_HFILE_OUTPUT_DIR should be ommited
+Example:
+
+
+2) using bulkloading
+
+To use bulkload,
+HDFS_BULK_HFILE_OUTPUT_DIR should be specified
+Example:
+
+
+# ------------------------------------------------------------------
+# command-line parameters                       # possible values
+# ------------------------------------------------------------------
+IMPORTERS_JAR=$1
+MODE=$2
 HDFS_SEQUENCE_FILE_INPUT_DIR=$3
-HDFS_BULK_HFILE_OUTPUT_DIR=$4
-HBASE_TABLENAME=$5
+HBASE_TABLENAME=$4
+BULKLOAD_MAPPER=$5
+
+# ------------------------------------------------------------------
+# optional command-line parameters		# possible values
+# ------------------------------------------------------------------
+HBASE_FULL_COLUMN_NAME=$6
+HDFS_BULK_HFILE_OUTPUT_DIR=$7
 
 # create hfile (mapreduce job)
-if [ "${MODE}" = "hfile" ] || [ "${MODE}" = "all" ]; then
+if [ "${MODE}" = "hfileput" ] || [ "${MODE}" = "all" ]; then
         hadoop fs -rm -r ${HDFS_BULK_HFILE_OUTPUT_DIR}
-        hadoop jar ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.hbase.DocumentWrapperSequenceFileToHBase -D mapreduce.map.class=DocumentWrapperToHBasePutMapper -D bulk.output=${HDFS_BULK_HFILE_OUTPUT_DIR} ${HDFS_SEQUENCE_FILE_INPUT_DIR} ${HBASE_TABLENAME}
+        hadoop jar ${IMPORTERS_JAR} pl.edu.icm.coansys.importers.io.writers.hbase.DocumentWrapperSequenceFileToHBase -D hbase.table.full.column.name=${HBASE_FULL_COLUMN_NAME} -D mapreduce.map.class=${BULKLOAD_MAPPER} -D bulk.output=${HDFS_BULK_HFILE_OUTPUT_DIR} ${HDFS_SEQUENCE_FILE_INPUT_DIR} ${HBASE_TABLENAME}
         hadoop fs -chmod -R 777 ${HDFS_BULK_HFILE_OUTPUT_DIR}
 fi
 
