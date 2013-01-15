4870dbc5991323dc73313785b5d3158610fcac54
Artur Czeczko <a.czeczko@icm.edu.pl>
2013-01-15 13:20:35 +0100

    New protocol buffers schema and changes needed in java files

--- disambiguation-author/src/main/java/pl/edu/icm/coansys/disambiguation/author/features/extractors/DocumentProto2ClassifCodeExtractor.java
+++ disambiguation-author/src/main/java/pl/edu/icm/coansys/disambiguation/author/features/extractors/DocumentProto2ClassifCodeExtractor.java
@@ -16,24 +16,26 @@ import pl.edu.icm.coansys.importers.models.DocumentProtos.ClassifCode;
 import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentMetadata;
 
 /**
- * The {@link Extractor} from {@link DocumentProtos.DocumentMetadata} (commit 33ad120f11eb430d450) to a feature value list. 
- * Please read the description of the {@link DocumentBased} interface.   
- * 
+ * The {@link Extractor} from {@link DocumentProtos.DocumentMetadata} (commit
+ * 33ad120f11eb430d450) to a feature value list. Please read the description of
+ * the {@link DocumentBased} interface.
+ *
  * @author pdendek
  * @version 1.0
  * @since 2012-08-07
  */
-public class DocumentProto2ClassifCodeExtractor implements Extractor<DocumentMetadata>,DocumentBased{
+public class DocumentProto2ClassifCodeExtractor implements Extractor<DocumentMetadata>, DocumentBased {
 
-	@SuppressWarnings("unused")
-	private static Logger logger = LoggerFactory.getLogger(new DisambiguationJob_Toy().getClass());
-	
-	public List<String> extract(DocumentMetadata input, String... auxil) {
-		DocumentMetadata dm = (DocumentMetadata) input;
-		ArrayList<String> ret = new ArrayList<String>();
-		for(ClassifCode cc : dm.getClassifCodeList()){
-			ret.addAll(cc.getValueList());
-		}
-		return ret;
-	}
+    @SuppressWarnings("unused")
+    private static Logger logger = LoggerFactory.getLogger(new DisambiguationJob_Toy().getClass());
+
+    @Override
+    public List<String> extract(DocumentMetadata input, String... auxil) {
+        DocumentMetadata dm = (DocumentMetadata) input;
+        ArrayList<String> ret = new ArrayList<String>();
+        for (ClassifCode cc : dm.getBasicMetadata().getClassifCodeList()) {
+            ret.addAll(cc.getValueList());
+        }
+        return ret;
+    }
 }
--- disambiguation-author/src/main/java/pl/edu/icm/coansys/disambiguation/author/features/extractors/DocumentProto2EmailPrefixExtractor.java
+++ disambiguation-author/src/main/java/pl/edu/icm/coansys/disambiguation/author/features/extractors/DocumentProto2EmailPrefixExtractor.java
@@ -18,35 +18,37 @@ import pl.edu.icm.coansys.importers.models.DocumentProtos.Author;
 import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentMetadata;
 
 /**
- * The {@link Extractor} from {@link DocumentProtos.DocumentMetadata} (commit 33ad120f11eb430d450) to a feature value list. 
- * Please read the description of the {@link AuthorBased} interface.
- * 
+ * The {@link Extractor} from {@link DocumentProtos.DocumentMetadata} (commit
+ * 33ad120f11eb430d450) to a feature value list. Please read the description of
+ * the {@link AuthorBased} interface.
+ *
  * @author pdendek
  * @version 1.0
  * @since 2012-08-07
  */
 public class DocumentProto2EmailPrefixExtractor implements Extractor<DocumentMetadata>, AuthorBased {
-	
-	private static Logger logger = LoggerFactory.getLogger(new DisambiguationJob_Toy().getClass());
-	
-	public List<String> extract(DocumentMetadata input,  String... auxil) {
-		String authId = auxil[0];
-		DocumentMetadata dm = (DocumentMetadata) input;
-		ArrayList<String> ret = new ArrayList<String>();
-		
-		for(Author a : dm.getAuthorList()){
-			if(a.getKey() == authId){
-				String email = a.getEmail();
-	    		Matcher matcher = Pattern.compile("[^a-zA-Z]_\\.\\-0-9").matcher(email);
-	    		if(matcher.find()){
-	    			ret.add(email.substring(0, matcher.start()));
-	    			return ret;  
-	    		}else{
-	    			logger.error("Email \""+a.getEmail()+"\" could not be parsed");
-	    		}
-			}
-		}
-		return ret;
-		
-	}
+
+    private static Logger logger = LoggerFactory.getLogger(new DisambiguationJob_Toy().getClass());
+
+    @Override
+    public List<String> extract(DocumentMetadata input, String... auxil) {
+        String authId = auxil[0];
+        DocumentMetadata dm = (DocumentMetadata) input;
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for (Author a : dm.getBasicMetadata().getAuthorList()) {
+            if (a.getKey().equals(authId)) {
+                String email = a.getEmail();
+                Matcher matcher = Pattern.compile("[^a-zA-Z]_\\.\\-0-9").matcher(email);
+                if (matcher.find()) {
+                    ret.add(email.substring(0, matcher.start()));
+                    return ret;
+                } else {
+                    logger.error("Email \"" + a.getEmail() + "\" could not be parsed");
+                }
+            }
+        }
+        return ret;
+
+    }
 }
