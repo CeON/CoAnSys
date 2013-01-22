5566964de2c0ed6e868d98db7e7b085fa03dd0cd
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-01-22 15:52:03 +0100

    command line args

--- citations/src/main/scala/pl/edu/icm/coansys/citations/tools/cermine/CermineParserExecutor.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/tools/cermine/CermineParserExecutor.scala
@@ -7,22 +7,28 @@ package pl.edu.icm.coansys.citations.tools.cermine
 import java.io.{FileInputStream, File}
 import pl.edu.icm.cermine.bibref.CRFBibReferenceParser
 import pl.edu.icm.coansys.citations.tools.cermine.util._
+import pl.edu.icm.coansys.citations.util.dataset_readers.findAndCollapseStringName
 import pl.edu.icm.cermine.bibref.parsing.tools.CitationUtils
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
  */
 object CermineParserExecutor {
-  def main() {
-    val modelFile = ""
-    val inFile = ""
-    val outFile = ""
+  def main(args: Array[String]) {
+    val modelFile = args(0)
+    val inFile = args(1)
+    val outFile = args(2)
 
     val parser = new CRFBibReferenceParser(new FileInputStream(modelFile))
 
     val citations = extractMixedCitationsFromXmls(Some(new File(inFile)))
     val citationStrings = citations.map(_.getValue)
-    val parsedCitations = citationStrings.map(cit => CitationUtils.bibEntryToNLM(parser.parseBibReference(cit)))
+    val parsedCitations = citationStrings.map {
+      cit =>
+        val elem = CitationUtils.bibEntryToNLM(parser.parseBibReference(cit))
+        findAndCollapseStringName(elem)
+        elem
+    }
 
     writeCitationsToXml(new File(outFile), parsedCitations)
   }
