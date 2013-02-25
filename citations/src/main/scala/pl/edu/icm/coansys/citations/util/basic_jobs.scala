1b406c1a73775ea5c87e77d3bc7bc53ad84b7d21
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-02-25 12:48:11 +0100

    map-reduce sort: file name confict resolution

--- citations/src/main/scala/pl/edu/icm/coansys/citations/util/basic_jobs.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/util/basic_jobs.scala
@@ -5,12 +5,13 @@
 package pl.edu.icm.coansys.citations.util
 
 import com.nicta.scoobi.Scoobi._
+import org.apache.hadoop.fs.{Path, FileSystem}
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
  */
 object basic_jobs {
-  def sort[K, V](indexFile: String)
+  def sort[K, V](indexFile: String, tmpFile: String = "")
                 (implicit conf: ScoobiConfiguration,
                  evidence$1: Manifest[K],
                  evidence$2: WireFormat[K],
@@ -18,9 +19,12 @@ object basic_jobs {
                  evidence$4: Manifest[V],
                  evidence$5: WireFormat[V],
                  evidence$6: SeqSchema[V]) {
+    val fs = FileSystem.get(conf)
+    val tmp = if (!tmpFile.isEmpty) tmpFile else indexFile + "_tmp"
+    fs.rename(new Path(indexFile), new Path(tmp))
     val maxRed = conf.getMaxReducers
     conf.setMaxReducers(1)
-    val entities = convertFromSequenceFile[K, V](indexFile)
+    val entities = convertFromSequenceFile[K, V](tmpFile)
     persist(convertToSequenceFile[K, V](entities.groupBy(_ => 0).values.flatten, indexFile))
     conf.setMaxReducers(maxRed)
   }
