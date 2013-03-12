f574f264121af83cb62a9a8e63bdc40bcfc05eff
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-03-12 15:40:30 +0100

    cleanup

--- citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/MixedCitationExtractor.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/MixedCitationExtractor.scala
@@ -5,8 +5,7 @@
 package pl.edu.icm.coansys.citations.jobs
 
 import com.nicta.scoobi.application.ScoobiApp
-import pl.edu.icm.coansys.citations.util.{XPathEvaluator, BytesConverter}
-import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentWrapper
+import pl.edu.icm.coansys.citations.util.XPathEvaluator
 import com.nicta.scoobi.InputsOutputs._
 import org.apache.commons.io.IOUtils
 import com.nicta.scoobi.Persist._
@@ -23,8 +22,7 @@ object MixedCitationExtractor extends ScoobiApp {
 
   def run() {
     configuration.set("mapred.task.timeout", 60 * 60 * 1000)
-    implicit val wrapperConverter =
-      new BytesConverter[DocumentWrapper](_.toByteString.toByteArray, DocumentWrapper.parseFrom(_))
+
     val nlmUri = args(0)
     val outUri = args(1)
     val nlms = convertFromSequenceFile[String, String](List(nlmUri))
