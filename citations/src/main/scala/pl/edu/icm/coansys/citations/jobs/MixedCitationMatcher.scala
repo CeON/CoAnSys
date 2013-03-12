c50c3e3c63e848a77965bf6de7c3a2c46db34f19
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-03-12 15:52:35 +0100

    adding MixedCitationMatcher

---
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/MixedCitationMatcher.scala
@@ -0,0 +1,40 @@
+/*
+ * (C) 2010-2012 ICM UW. All rights reserved.
+ */
+
+package pl.edu.icm.coansys.citations.jobs
+
+import com.nicta.scoobi.application.ScoobiApp
+import com.nicta.scoobi.InputsOutputs._
+import pl.edu.icm.coansys.citations.util.XPathEvaluator
+import org.apache.commons.io.IOUtils
+import com.nicta.scoobi.Persist._
+import pl.edu.icm.coansys.citations.util.AugmentedDList.augmentDList
+import pl.edu.icm.coansys.citations.indices.EntityIndex
+
+/**
+ * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+ */
+class MixedCitationMatcher extends ScoobiApp {
+  override def upload = false
+
+  def run() {
+    configuration.set("mapred.task.timeout", 60 * 60 * 1000)
+
+    val indexUri = args(0)
+    val citationsUri = args(1)
+    val outUri = args(2)
+    val cits = convertValueFromSequenceFile[String](List(citationsUri))
+    val res = cits.flatMapWithResource(new EntityIndex(indexUri)) {
+      case (index, xml) =>
+        val eval = XPathEvaluator.fromInputStream(IOUtils.toInputStream(xml))
+        val pmid = eval( """/ref/mixed-citation/pub-id[@pub-id-type='pmid']""")
+        if (!pmid.isEmpty)
+          Some((xml, index.getEntityById("doc-" + pmid)))
+        else
+          None
+    }
+
+    persist(convertToSequenceFile(res, outUri, overwrite = true))
+  }
+}
