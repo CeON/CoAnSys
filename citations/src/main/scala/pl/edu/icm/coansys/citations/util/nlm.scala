b099cb23b18d5089adffbc6719c5c832ff25af2c
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-03-12 11:05:33 +0100

    XPathEvaluator extraction

--- citations/src/main/scala/pl/edu/icm/coansys/citations/util/nlm.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/util/nlm.scala
@@ -4,12 +4,9 @@
 
 package pl.edu.icm.coansys.citations.util
 
-import java.io.{StringReader, FileInputStream, InputStream, File}
+import java.io.{FileInputStream, InputStream, File}
 import pl.edu.icm.coansys.importers.models.DocumentProtos._
-import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.xpath.{XPathConstants, XPathFactory}
-import org.xml.sax.{InputSource, EntityResolver}
-import org.w3c.dom.{Node, NodeList}
+import org.w3c.dom.Node
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -19,34 +16,6 @@ object nlm {
     pubmedNlmToProtoBuf(new FileInputStream(f))
 
   def pubmedNlmToProtoBuf(is: InputStream): DocumentWrapper = {
-    class XPathEvaluator(private val obj: Any) extends (String => String) {
-
-      private val evaluator = XPathFactory.newInstance().newXPath()
-
-      def apply(xpath: String): String =
-        evaluator.evaluate(xpath, obj)
-
-      def asNode(xpath: String) =
-        evaluator.evaluate(xpath, obj, XPathConstants.NODE).asInstanceOf[Node]
-
-      def asNodes(xpath: String) = {
-        val nodes = evaluator.evaluate(xpath, obj, XPathConstants.NODESET).asInstanceOf[NodeList]
-        for (i <- 0 until nodes.getLength)
-        yield nodes.item(i)
-      }
-    }
-
-    object XPathEvaluator {
-      def fromInputStream(is: InputStream) = {
-        val docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        docBuilder.setEntityResolver(new EntityResolver {
-          def resolveEntity(publicId: String, systemId: String) = new InputSource(new StringReader(""))
-        })
-        val doc = docBuilder.parse(is)
-        new XPathEvaluator(doc)
-      }
-    }
-
     def authorBuilderFromNameNode(nameNode: Node): Author.Builder = {
       val nameEval = new XPathEvaluator(nameNode)
       val author = Author.newBuilder()
