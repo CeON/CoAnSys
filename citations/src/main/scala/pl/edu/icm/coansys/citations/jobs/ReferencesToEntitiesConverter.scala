b4717b2f2a45da4ce8ce67229c041b0eb9f3d16a
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-04-22 17:29:09 +0200

    fixes

--- citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/ReferencesToEntitiesConverter.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/ReferencesToEntitiesConverter.scala
@@ -20,16 +20,16 @@ object ReferencesToEntitiesConverter extends ScoobiApp {
   override def upload = false
 
   def run() {
-    var parser: (=> BibReferenceParser[BibEntry] with NoOpClose) = null
+    var parser: (()=> BibReferenceParser[BibEntry] with NoOpClose) = null
     var inUri: String = null
     var outUri: String = null
     if(args(0) == "-model") {
-      parser = new CRFBibReferenceParser(args(1)) with NoOpClose
+      parser = () => new CRFBibReferenceParser(args(1)) with NoOpClose
       inUri = args(2)
       outUri = args(3)
     }
     else {
-      parser = new CRFBibReferenceParser(
+      parser = () => new CRFBibReferenceParser(
         this.getClass.getResourceAsStream("/pl/edu/icm/cermine/bibref/acrf-small.ser.gz")) with NoOpClose
       inUri = args(0)
       outUri = args(1)
@@ -39,9 +39,9 @@ object ReferencesToEntitiesConverter extends ScoobiApp {
     implicit val rmConverter = new BytesConverter[ReferenceMetadata](_.toByteArray, ReferenceMetadata.parseFrom(_))
     val entities = convertValueFromSequenceFile[DocumentWrapper](inUri)
       .flatMap[ReferenceMetadata](b => b.getDocumentMetadata.getReferenceList.toIterable)
-      .flatMapWithResource(parser) {
-      case (parser, meta) if !meta.getRawCitationText.isEmpty =>
-        Some(MatchableEntity.fromUnparsedReferenceMetadata(parser, meta))
+      .flatMapWithResource(parser()) {
+      case (the_parser, meta) if !meta.getRawCitationText.isEmpty =>
+        Some(MatchableEntity.fromUnparsedReferenceMetadata(the_parser, meta))
       case _ =>
         None
     }
