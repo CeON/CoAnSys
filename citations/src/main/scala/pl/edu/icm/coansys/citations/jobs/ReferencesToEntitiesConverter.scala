5382170fc9162a806a0fc8baea4fdb9d65250553
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-04-22 15:36:27 +0200

    workflow rebuilding

--- citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/ReferencesToEntitiesConverter.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/ReferencesToEntitiesConverter.scala
@@ -10,7 +10,8 @@ import pl.edu.icm.coansys.citations.util.AugmentedDList.augmentDList
 import pl.edu.icm.coansys.citations.util.{NoOpClose, BytesConverter}
 import pl.edu.icm.coansys.importers.models.DocumentProtos.{ReferenceMetadata, DocumentWrapper}
 import pl.edu.icm.coansys.citations.data.MatchableEntity
-import pl.edu.icm.cermine.bibref.CRFBibReferenceParser
+import pl.edu.icm.cermine.bibref.{BibReferenceParser, CRFBibReferenceParser}
+import pl.edu.icm.cermine.bibref.model.BibEntry
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -19,16 +20,26 @@ object ReferencesToEntitiesConverter extends ScoobiApp {
   override def upload = false
 
   def run() {
-    assert(args(0) == "-model")
-    val parserModel = Some(args(1))
-    val inUri = args(2)
-    val outUri = args(3)
+    var parser: (=> BibReferenceParser[BibEntry] with NoOpClose) = null
+    var inUri: String = null
+    var outUri: String = null
+    if(args(0) == "-model") {
+      parser = new CRFBibReferenceParser(args(1)) with NoOpClose
+      inUri = args(2)
+      outUri = args(3)
+    }
+    else {
+      parser = new CRFBibReferenceParser(
+        this.getClass.getResourceAsStream("/pl/edu/icm/cermine/bibref/acrf-small.ser.gz")) with NoOpClose
+      inUri = args(0)
+      outUri = args(1)
+    }
 
     implicit val dwConverter = new BytesConverter[DocumentWrapper](_.toByteArray, DocumentWrapper.parseFrom(_))
     implicit val rmConverter = new BytesConverter[ReferenceMetadata](_.toByteArray, ReferenceMetadata.parseFrom(_))
     val entities = convertValueFromSequenceFile[DocumentWrapper](inUri)
       .flatMap[ReferenceMetadata](b => b.getDocumentMetadata.getReferenceList.toIterable)
-      .flatMapWithResource(new CRFBibReferenceParser(parserModel.get) with NoOpClose) {
+      .flatMapWithResource(parser) {
       case (parser, meta) if !meta.getRawCitationText.isEmpty =>
         Some(MatchableEntity.fromUnparsedReferenceMetadata(parser, meta))
       case _ =>
