7f5432c5278205c900701a91c2bdbf71649502f0
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-04-29 16:25:31 +0200

    heuristic stats

--- citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/auxiliary/HeuristicStatsWrong.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/auxiliary/HeuristicStatsWrong.scala
@@ -2,38 +2,35 @@ package pl.edu.icm.coansys.citations.jobs.auxiliary
 
 import com.nicta.scoobi.application.ScoobiApp
 import com.nicta.scoobi.Scoobi
+import com.nicta.scoobi.InputsOutputs._
 import com.nicta.scoobi.Persist._
-import pl.edu.icm.coansys.citations.util.{XPathEvaluator, nlm}
-import org.apache.commons.io.IOUtils
-import pl.edu.icm.coansys.citations.data.MatchableEntity
 import org.apache.commons.lang.StringUtils
+import pl.edu.icm.coansys.citations.util.AugmentedDList.augmentDList
+import pl.edu.icm.coansys.citations.indices.EntityIndex
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
  */
 object HeuristicStatsWrong extends ScoobiApp {
   def run() {
-    val inUri = args(0)
+    val indexUri = args(0)
+    val inUri = args(1)
+    val outUri = args(2)
 
     val results = Scoobi.convertFromSequenceFile[String, String](inUri)
-      .flatMap { case (k, v) =>
+      .flatMapWithResource(new EntityIndex(indexUri)) { case (index, (k, v)) =>
         val parts = v.split("\n", 2)
         val ids = parts(0).split(" ").filterNot(StringUtils.isEmpty).map(_.substring(4))
-        val xmlString = parts(1)
+        val xml = parts(1)
         if (ids contains k) {
           None
         } else {
-          val eval = XPathEvaluator.fromInputStream(IOUtils.toInputStream(xmlString))
-          val ref = nlm.referenceMetadataBuilderFromNode(eval.asNode("."))
-          val authorTokens = MatchableEntity.fromReferenceMetadata(ref.build()).normalisedAuthorTokens
-          Some((1, ids.length, authorTokens.size))
+          val srcDoc = index.getEntityById("doc_" + k)
+          val dstDocs = ids.map(id => index.getEntityById("doc_" + id))
+          Some((srcDoc.toDebugString, xml + "\n\n" + dstDocs.map(_.toDebugString).mkString("\n")))
         }
       }
-      .reduce{ case ((count1, idsLen1, tokLen1),(count2, idsLen2, tokLen2)) =>
-        (count1 + count2, idsLen1 + idsLen2, tokLen1 + tokLen2)
-      }
 
-    val persisted = persist(results)
-    println(persisted)
+    persist(convertToSequenceFile(results, outUri, overwrite = true))
   }
 }
