390d581d48ef8ca1ec8b92c97f44b42ea449d8da
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-05-13 14:21:14 +0200

    faster evaluator

--- citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/auxiliary/Evaluator.scala
+++ citations/src/main/scala/pl/edu/icm/coansys/citations/jobs/auxiliary/Evaluator.scala
@@ -8,13 +8,14 @@ import com.nicta.scoobi.Persist._
 import org.apache.commons.lang.StringUtils
 import pl.edu.icm.coansys.citations.util.AugmentedDList.augmentDList
 import pl.edu.icm.coansys.citations.indices.{AuthorIndex, EntityIndex}
-import pl.edu.icm.coansys.citations.util.{libsvm, nlm, XPathEvaluator}
+import pl.edu.icm.coansys.citations.util.{NoOpClose, libsvm, nlm, XPathEvaluator}
 import org.apache.commons.io.IOUtils
 import pl.edu.icm.coansys.citations.data.{SimilarityMeasurer, MatchableEntity}
 import pl.edu.icm.cermine.tools.classification.features.FeatureVectorBuilder
 import pl.edu.icm.coansys.citations.data.feature_calculators._
 import com.nicta.scoobi.core.DList
 import pl.edu.icm.coansys.citations.util.matching._
+import com.nicta.scoobi.lib.Relational
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -25,48 +26,39 @@ object Evaluator extends ScoobiApp {
     val inUri = args(1)
     val outUri = args(2)
 
-    val results = Scoobi.convertFromSequenceFile[String, String](inUri)
-      .mapWithResource(new ScalaObject {
-      val index = new EntityIndex(indexUri)
-      val similarityMeasurer = new SimilarityMeasurer
-
-      def close() {
-        index.close()
-      }
-    }) {
-      case (res, (k, v)) =>
+    val indexList = Scoobi.convertFromSequenceFile[String, MatchableEntity](indexUri)
+    val heuristic = Scoobi.convertFromSequenceFile[String, String](inUri)
+    // citId->citData
+    val citations = heuristic.map {
+      case (k, v) =>
         val parts = v.split("\n", 2)
-        val ids = parts(0).split(" ").filterNot(StringUtils.isEmpty).map(_.substring(4)).toSet
         val xmlString = parts(1)
         val eval = XPathEvaluator.fromInputStream(IOUtils.toInputStream(xmlString))
         val ref = nlm.referenceMetadataBuilderFromNode(eval.asNode(".")).build()
-        val srcCit = MatchableEntity.fromReferenceMetadata(ref)
-        val dstDocs = ids.toList.map(id => (id, res.index.getEntityById("doc_" + id)))
+        val cit = MatchableEntity.fromReferenceMetadata(ref)
+        (k, cit)
+    }
+    // doc_destDocId->correctId
+    val proposed = heuristic.flatMap {
+      case (k, v) =>
+        val parts = v.split("\n", 2)
+        val ids = parts(0).split(" ").filterNot(StringUtils.isEmpty).toSet
+        ids zip Stream.continually(k)
+    }
 
-        if (dstDocs.isEmpty) {
-          0
-        } else {
-          val featureVectorBuilder = new FeatureVectorBuilder[MatchableEntity, MatchableEntity]
-          featureVectorBuilder.setFeatureCalculators(List(
-            AuthorTrigramMatchFactor,
-            AuthorTokenMatchFactor,
-            PagesMatchFactor,
-            SourceMatchFactor,
-            TitleMatchFactor,
-            YearMatchFactor))
-
-
-          val sims = dstDocs.map {
-            case (id, dst) =>
-              (id, res.similarityMeasurer.similarity(srcCit, dst))
-          }
-
-          val best = sims.maxBy(_._2)._1
-          if (best == k)
-            1
-          else
-            0
-        }
+    //citId->heuristicly matched data
+    val withData = Relational.joinLeft(proposed, indexList).map {
+      case (prefixedDest, (src, Some(entity))) =>
+        //val dest = prefixedDest.substring(4)
+        (src, entity)
+    }.groupByKey[String, MatchableEntity]
+    val results = Relational.joinLeft(citations, withData).flatMapWithResource(new SimilarityMeasurer with NoOpClose) {
+      case (measurer, (key, (cit, Some(iter)))) =>
+        val best = iter.maxBy(measurer.similarity(_, cit))
+        if (best.id.substring(4) != key)
+          Some(cit.toDebugString, best.toDebugString)
+        else
+          None
     }
 
     persist(toTextFile(results, outUri, overwrite = true))
