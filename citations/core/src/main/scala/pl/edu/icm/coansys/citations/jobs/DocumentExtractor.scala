6ca42d78f3b6999c9484f8b55807901b1e248246
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-05-24 09:43:40 +0200

    jar upload test

--- citations/core/src/main/scala/pl/edu/icm/coansys/citations/jobs/DocumentExtractor.scala
+++ citations/core/src/main/scala/pl/edu/icm/coansys/citations/jobs/DocumentExtractor.scala
@@ -3,6 +3,7 @@ package pl.edu.icm.coansys.citations.jobs
 import com.nicta.scoobi.Scoobi._
 import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentWrapper
 import pl.edu.icm.coansys.citations.data.WireFormats._
+import java.io.File
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -10,6 +11,12 @@ import pl.edu.icm.coansys.citations.data.WireFormats._
 
 
 object DocumentExtractor extends ScoobiApp {
+  val myJars = System.getProperty("java.class.path").split(File.pathSeparator)
+    .filter(_.toLowerCase.contains(File.separator + "distcache" + File.separator))
+    .filter(_.toLowerCase.endsWith(".jar"))
+
+  configuration.addJars(myJars)
+
   lazy val documentIdPrefix = "doc_"
 
   def extractDocuments(in: DList[DocumentWrapper]) =
--- citations/core/src/main/scala/pl/edu/icm/coansys/citations/jobs/ReferenceExtractor.scala
+++ citations/core/src/main/scala/pl/edu/icm/coansys/citations/jobs/ReferenceExtractor.scala
@@ -4,12 +4,19 @@ import collection.JavaConversions._
 import com.nicta.scoobi.Scoobi._
 import pl.edu.icm.coansys.importers.models.DocumentProtos.DocumentWrapper
 import pl.edu.icm.coansys.citations.data.WireFormats._
+import java.io.File
 
 /**
   * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
   */
 object ReferenceExtractor extends ScoobiApp {
-   lazy val citationIdPrefix = "cit_"
+  val myJars = System.getProperty("java.class.path").split(File.pathSeparator)
+    .filter(_.toLowerCase.contains(File.separator + "distcache" + File.separator))
+    .filter(_.toLowerCase.endsWith(".jar"))
+
+  configuration.addJars(myJars)
+
+  lazy val citationIdPrefix = "cit_"
 
    def extractReferences(docs: DList[DocumentWrapper]) =
      docs.filterNot(_.getDocumentMetadata.getKey.isEmpty)
