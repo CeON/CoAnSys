febe78c881a5949fffd1de9821e6ba18ef5d0f1a
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-07-19 14:58:34 +0200

    do not use too many files

--- citations/core/src/main/scala/pl/edu/icm/coansys/citations/util/MyScoobiApp.scala
+++ citations/core/src/main/scala/pl/edu/icm/coansys/citations/util/MyScoobiApp.scala
@@ -1,9 +1,12 @@
 package pl.edu.icm.coansys.citations.util
 
 import com.nicta.scoobi.application.ScoobiApp
+import com.nicta.scoobi.impl.ScoobiConfiguration._
+import com.nicta.scoobi.impl.ScoobiConfigurationImpl._
 import com.nicta.scoobi.core.ScoobiConfiguration
 import org.apache.hadoop.filecache.DistributedCache
 import org.apache.hadoop.fs.Path
+import java.io.File
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -12,9 +15,10 @@ abstract class MyScoobiApp extends ScoobiApp {
   override def upload = true
 
   override def configureJars(implicit configuration: ScoobiConfiguration) = {
-    super.configureJars
     uploadedJars.foreach(path =>
-      DistributedCache.addFileToClassPath(new Path(path.toUri.getPath), configuration.configuration))
-    configuration.configuration
+      DistributedCache.addFileToClassPath(new Path(path.toUri.getPath), configuration))
+
+    // add new jars to the classpath and make sure that values are still unique for cache files and classpath entries
+    configuration.addValues("mapred.classpath", jars.map(j => libjarsDirectory + (new File(j.getFile).getName)), ":")
   }
 }
