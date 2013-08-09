3732fb3dbd2aa4105cb4bfa8979bdb6c4b19869e
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-08-09 15:30:31 +0200

    better ids support

---
+++ citations/core/src/test/scala/pl/edu/icm/coansys/citations/data/entity_id/DocEntityIdTest.scala
@@ -0,0 +1,34 @@
+/*
+ * This file is part of CoAnSys project.
+ * Copyright (c) 2012-2013 ICM-UW
+ *
+ * CoAnSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CoAnSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public Licensealong with CoAnSys. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.edu.icm.coansys.citations.data.entity_id
+
+import org.testng.Assert._
+import org.testng.annotations.Test
+
+/**
+ * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+ */
+class DocEntityIdTest {
+    @Test(groups = Array("fast"))
+    def basicTest() {
+        val docId = "id12345"
+        val id = DocEntityId.fromString(DocEntityId(docId).toString())
+
+      assertEquals(docId, id.documentId)
+    }
+}
