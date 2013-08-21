fe9eec65019fef47310f00c9c7136c3061f232dc
pdendek <pdendek@icm.edu.pl>
2013-08-21 14:08:17 +0200

    main 'test' classes to JUnits

--- disambiguation/src/main/java/pl/edu/icm/coansys/disambiguation/clustering/strategies/SingleLinkageHACStrategy_LessThenZero_Or_Max.java
+++ disambiguation/src/main/java/pl/edu/icm/coansys/disambiguation/clustering/strategies/SingleLinkageHACStrategy_LessThenZero_Or_Max.java
@@ -18,8 +18,6 @@
 
 package pl.edu.icm.coansys.disambiguation.clustering.strategies;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -28,19 +26,6 @@ import org.slf4j.LoggerFactory;
  * @since 2012-08-07
  */
 public class SingleLinkageHACStrategy_LessThenZero_Or_Max extends SingleLinkageHACStrategy implements Cloneable {
-
-    private static final Logger log = LoggerFactory.getLogger(SingleLinkageHACStrategy_LessThenZero_Or_Max.class);
-
-    public static void main(String[] args) {
-        float[][] in = {{}, {15}, {46, 3}, {2, -18, -20}, {-100, -100, 3, -200}};
-        int[] out = new SingleLinkageHACStrategy_LessThenZero_Or_Max().clusterize(in);
-        StringBuilder sb = new StringBuilder("");
-        for (int i : out) {
-            sb.append(i).append("\t");
-        }
-        sb.append("\n");
-        log.info(sb.toString());
-    }
 
     @Override
     protected float SIM(float a, float b) {
