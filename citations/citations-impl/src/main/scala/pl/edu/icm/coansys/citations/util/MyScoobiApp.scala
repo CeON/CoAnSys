5f511a3e19441187ac0a49a0ed79839ce8f3a685
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-10-29 17:17:43 +0100

    update to Scoobi 0.7.3

--- citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/util/MyScoobiApp.scala
+++ citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/util/MyScoobiApp.scala
@@ -19,31 +19,11 @@
 package pl.edu.icm.coansys.citations.util
 
 import com.nicta.scoobi.application.ScoobiApp
-import com.nicta.scoobi.impl.ScoobiConfiguration._
-import com.nicta.scoobi.impl.ScoobiConfigurationImpl._
-import com.nicta.scoobi.core.ScoobiConfiguration
-import org.apache.hadoop.filecache.DistributedCache
-import org.apache.hadoop.fs.Path
-import java.io.File
 
 /**
  * ScoobiApp variant that doesn't add to Distributed Cache jars that are already there.
- * Will become obsolete with Scoobi 0.8, as it will be a default behaviour there.
+ * It became obsolete with Scoobi 0.7.3.
  *
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
  */
-abstract class MyScoobiApp extends ScoobiApp {
-  override def upload = true
-
-  override def configureJars(implicit configuration: ScoobiConfiguration) = {
-    val added =
-      Option(DistributedCache.getFileClassPaths(configuration)).map(_.toList).getOrElse(List())
-        .map(_.toUri.getPath).toSet
-    uploadedJars
-      .filterNot(path => added.contains(path.toUri.getPath))
-      .foreach(path => DistributedCache.addFileToClassPath(new Path(path.toUri.getPath), configuration))
-
-    // add new jars to the classpath and make sure that values are still unique for cache files and classpath entries
-    configuration.addValues("mapred.classpath", jars.map(j => libjarsDirectory + (new File(j.getFile).getName)), ":")
-  }
-}
+abstract class MyScoobiApp extends ScoobiApp {}
