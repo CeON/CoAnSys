4d2202e66cbd70077bd0aa5ff8e1b54041464b76
Artur Czeczko <a.czeczko@icm.edu.pl>
2013-12-02 11:53:41 +0100

    Added module statistics generator

---
+++ statistics-generator/statistics-generator-impl/src/main/java/pl/edu/icm/coansys/statisticsgenerator/conf/ConfigurationConstants.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of CoAnSys project.
+ * Copyright (c) 2012-2013 ICM-UW
+ * 
+ * CoAnSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * CoAnSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CoAnSys. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.edu.icm.coansys.statisticsgenerator.conf;
+
+/**
+ *
+ * @author Artur Czeczko <a.czeczko@icm.edu.pl>
+ */
+public class ConfigurationConstants {
+    public static final String PARTITIONS_PREFIX = "partitions_";
+    public static final String STATISTICS_PREFIX = "statistics_";
+
+    public static final String NAMES = "names";
+    public static final String CLASSES = "classes";
+    public static final String CLASSES_ARGS = "classes_args";
+    
+    public static final String CONF_FIELDS_SEPARATOR = ";";
+    public static final String CONF_PARAMS_SEPARATOR = "#";
+    
+    public static final String DEFAULT_PARTITION_CLASS = "EQUALS";
+}
