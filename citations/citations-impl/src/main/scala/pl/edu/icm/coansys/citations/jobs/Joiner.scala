a42dc43b6c2a49aa05c32cc71b79f03829407096
Mateusz Fedoryszak <matfed@icm.edu.pl>
2013-12-04 16:49:59 +0100

    two steps

--- citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/jobs/Joiner.scala
+++ citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/jobs/Joiner.scala
@@ -3,13 +3,13 @@ package pl.edu.icm.coansys.citations.jobs
 import org.apache.hadoop.conf.{Configuration, Configured}
 import org.apache.hadoop.util.{ToolRunner, Tool}
 import org.apache.hadoop.fs.{Path, FileSystem}
-import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.{Mapper, Job}
 import org.apache.hadoop.mapreduce.lib.input.{MultipleInputs, SequenceFileInputFormat, FileInputFormat}
 import org.apache.hadoop.mapreduce.lib.output.{SequenceFileOutputFormat, FileOutputFormat}
 import pl.edu.icm.coansys.citations.mappers.{AuthorIndexer, EntityAuthorTagger, HeuristicAdder}
 import org.apache.hadoop.io.{Writable, Text, BytesWritable}
 import pl.edu.icm.coansys.citations.data._
-import pl.edu.icm.coansys.citations.reducers.AuthorJoiner
+import pl.edu.icm.coansys.citations.reducers.{AuthorJoinerStepTwo, AuthorJoinerStepOne}
 
 /**
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
@@ -20,35 +20,63 @@ object Joiner extends Configured with Tool {
     val destUri = args(1)
     val outUri = args(2)
 
-    val conf = getConf
-    val fs = FileSystem.get(conf)
+    val confOne = getConf
 
-    conf.setInt("mapred.max.split.size", 5 * 1000 * 1000)
-    val job = new Job(conf, "Joiner")
-    job.setJarByClass(getClass)
+    confOne.setInt("mapred.max.split.size", 5 * 1000 * 1000)
+    val jobOne = new Job(confOne, "Joiner step one")
+    jobOne.setJarByClass(getClass)
 
-    job.setPartitionerClass(classOf[MarkedTextPartitioner])
-    job.setGroupingComparatorClass(classOf[MarkedTextGroupComparator])
-    job.setSortComparatorClass(classOf[MarkedTextSortComparator])
+    jobOne.setPartitionerClass(classOf[MarkedTextPartitioner])
+    jobOne.setGroupingComparatorClass(classOf[MarkedTextGroupComparator])
+    jobOne.setSortComparatorClass(classOf[MarkedTextSortComparator])
 
-    job.setReducerClass(classOf[AuthorJoiner])
+    jobOne.setReducerClass(classOf[AuthorJoinerStepOne])
 
-    job.setOutputKeyClass(classOf[BytesWritable])
-    job.setOutputValueClass(classOf[BytesWritable])
-    job.setOutputFormatClass(classOf[SequenceFileOutputFormat[BytesWritable, BytesWritable]])
+    jobOne.setOutputKeyClass(classOf[MarkedText])
+    jobOne.setOutputValueClass(classOf[BytesWritable])
+    jobOne.setOutputFormatClass(classOf[SequenceFileOutputFormat[MarkedText, BytesWritable]])
 
-    MultipleInputs.addInputPath(job, new Path(srcUri), classOf[SequenceFileInputFormat[Writable, BytesWritable]], classOf[EntityAuthorTagger])
-    MultipleInputs.addInputPath(job, new Path(destUri), classOf[SequenceFileInputFormat[Writable, BytesWritable]], classOf[AuthorIndexer])
+    MultipleInputs.addInputPath(jobOne, new Path(srcUri), classOf[SequenceFileInputFormat[Writable, BytesWritable]], classOf[EntityAuthorTagger])
+    MultipleInputs.addInputPath(jobOne, new Path(destUri), classOf[SequenceFileInputFormat[Writable, BytesWritable]], classOf[AuthorIndexer])
 
-    job.setMapOutputKeyClass(classOf[MarkedText])
-    job.setMapOutputValueClass(classOf[MarkedBytesWritable])
+    jobOne.setMapOutputKeyClass(classOf[MarkedText])
+    jobOne.setMapOutputValueClass(classOf[MarkedBytesWritable])
 
-    FileOutputFormat.setOutputPath(job, new Path(outUri))
+    FileOutputFormat.setOutputPath(jobOne, new Path(outUri+"_part1"))
 
-    if (job.waitForCompletion(true))
+    if (!jobOne.waitForCompletion(true))
+      return 1
+
+    val confTwo = getConf
+
+    confTwo.setInt("mapred.max.split.size", 5 * 1000 * 1000)
+    val jobTwo = new Job(confTwo, "Joiner step two")
+    jobTwo.setJarByClass(getClass)
+
+    jobTwo.setMapperClass(classOf[Mapper[MarkedText, BytesWritable, MarkedText, BytesWritable]])
+
+    jobTwo.setMapOutputKeyClass(classOf[MarkedText])
+    jobTwo.setMapOutputValueClass(classOf[BytesWritable])
+    jobTwo.setInputFormatClass(classOf[SequenceFileInputFormat[MarkedText, BytesWritable]])
+    FileInputFormat.addInputPath(jobTwo, new Path(outUri+"_part1"))
+
+    jobTwo.setPartitionerClass(classOf[MarkedTextPartitioner])
+    jobTwo.setGroupingComparatorClass(classOf[MarkedTextGroupComparator])
+    jobTwo.setSortComparatorClass(classOf[MarkedTextSortComparator])
+
+    jobTwo.setReducerClass(classOf[AuthorJoinerStepTwo])
+
+    jobTwo.setOutputKeyClass(classOf[MarkedText])
+    jobTwo.setOutputValueClass(classOf[BytesWritable])
+    jobTwo.setOutputFormatClass(classOf[SequenceFileOutputFormat[BytesWritable, BytesWritable]])
+    FileOutputFormat.setOutputPath(jobTwo, new Path(outUri))
+
+    if (jobTwo.waitForCompletion(true))
       0
     else
       1
+    
+    
   }
 
   def main(args: Array[String]) {
---
+++ citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/reducers/AuthorJoinerStepOne.scala
@@ -0,0 +1,33 @@
+package pl.edu.icm.coansys.citations.reducers
+
+import collection.JavaConversions._
+
+import org.apache.hadoop.mapreduce.Reducer
+import org.apache.hadoop.io.{BytesWritable, Text}
+import pl.edu.icm.coansys.citations.data.{MarkedBytesWritable, MarkedText}
+
+/**
+ * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+ */
+class AuthorJoinerStepOne extends Reducer[MarkedText, MarkedBytesWritable, MarkedText, BytesWritable] {
+  type Context = Reducer[MarkedText, MarkedBytesWritable, MarkedText, BytesWritable]#Context
+
+  val outMarkedKey = new MarkedText(marked = true)
+  val outUnmarkedKey = new MarkedText(marked = false)
+
+  override def reduce(key: MarkedText, values: java.lang.Iterable[MarkedBytesWritable], context: Context) {
+    var leftCount = 0
+    for (value <- values) {
+      if (value.isMarked.get()) {
+        outMarkedKey.text.set(key.toString + "_" + leftCount)
+        leftCount += 1
+        context.write(outMarkedKey, value.bytes)
+      } else {
+        for (i <- 0 until leftCount) {
+          outUnmarkedKey.text.set(key.toString + "_" + i)
+          context.write(outUnmarkedKey, value.bytes)
+        }
+      }
+    }
+  }
+}
