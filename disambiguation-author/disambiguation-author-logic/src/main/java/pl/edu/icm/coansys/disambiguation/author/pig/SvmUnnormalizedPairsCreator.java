8b0fa5b60cba25cf00ef73807211b127bbd96ada
Michał Woś <mwos@icm.edu.pl>
2013-12-13 02:36:24 +0100

    SvmUnnormalizedPairsCreator critical issue reported by Sonar fixed

--- disambiguation-author/disambiguation-author-logic/src/main/java/pl/edu/icm/coansys/disambiguation/author/pig/SvmUnnormalizedPairsCreator.java
+++ disambiguation-author/disambiguation-author-logic/src/main/java/pl/edu/icm/coansys/disambiguation/author/pig/SvmUnnormalizedPairsCreator.java
@@ -39,7 +39,6 @@ import org.slf4j.LoggerFactory;
 
 import pl.edu.icm.coansys.disambiguation.author.features.disambiguators.Disambiguator;
 import pl.edu.icm.coansys.disambiguation.author.features.disambiguators.Intersection;
-import pl.edu.icm.coansys.disambiguation.idgenerators.UuIdGenerator;
 
 public class SvmUnnormalizedPairsCreator  extends EvalFunc<DataBag> {
 
@@ -67,7 +66,7 @@ public class SvmUnnormalizedPairsCreator  extends EvalFunc<DataBag> {
 			return null;
 		}
 		
-		String sname = (String) tuple.get(0);
+		// String sname = (String) tuple.get(0);
 		DataBag contribs = (DataBag) tuple.get(1); 
 		
 		Tuple[] contribsT = new Tuple[(int) contribs.size()]; 
