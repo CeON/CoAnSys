899fd588f3f822a5af1f69bae07cf4a3923f7297
Mateusz Fedoryszak <matfed@icm.edu.pl>
2014-04-16 13:34:35 +0200

    modular architecture for heuristic workflow

--- citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/hashers/OptimisticCitationHashGenerator.scala
+++ citations/citations-impl/src/main/scala/pl/edu/icm/coansys/citations/hashers/OptimisticCitationHashGenerator.scala
@@ -16,45 +16,17 @@
  * along with CoAnSys. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package pl.edu.icm.coansys.citations.mappers
+package pl.edu.icm.coansys.citations.hashers
 
-import org.apache.hadoop.io.{BytesWritable, Writable}
-import org.apache.hadoop.mapreduce.Mapper
-import pl.edu.icm.coansys.citations.data.{MarkedBytesWritable, MarkedText, MatchableEntity}
+import pl.edu.icm.coansys.citations.data.MatchableEntity
 import pl.edu.icm.coansys.citations.util.misc._
-import pl.edu.icm.ceon.scala_commons.collections
 import scala.util.Try
+import pl.edu.icm.ceon.scala_commons.collections
 
 /**
- * Takes citation entity as a value and generates (hash, entity id) pairs.
- *
  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
  */
-class OptimisticCitationHashGenerator extends Mapper[Writable, BytesWritable, MarkedText, MarkedText] {
-  type Context = Mapper[Writable, BytesWritable, MarkedText, MarkedText]#Context
-  val outKey = new MarkedText(marked = true)
-  val outValue = new MarkedText(marked = true)
-
-  override def setup(context: Context) {
-    val marked = context.getConfiguration.getBoolean("coansys.citations.mark.citations", true)
-    outKey.isMarked.set(marked)
-    outValue.isMarked.set(marked)
-  }
-
-  override def map(key: Writable, value: BytesWritable, context: Context) {
-    val entity = MatchableEntity.fromBytes(value.copyBytes())
-
-    val hashes = OptimisticCitationHashGenerator.generate(entity)
-
-    outValue.text.set(entity.id)
-    hashes.foreach {hash =>
-      outKey.text.set(hash)
-      context.write(outKey, outValue)
-    }
-  }
-}
-
-object OptimisticCitationHashGenerator {
+class OptimisticCitationHashGenerator extends HashGenerator {
   def generate(entity: MatchableEntity) = for {
     author <- lettersNormaliseTokenise(entity.author).filterNot(stopWords).distinct.take(4)
     year <- digitsNormaliseTokenise(entity.year).filter(_.length == 4).flatMap(x => Try(x.toInt).toOption).filter(x => x < 2050 && x > 1900)
---
+++ citations/citations-impl/src/test/scala/pl/edu/icm/coansys/citations/hashers/DocumentHashGeneratorTest.scala
@@ -0,0 +1,46 @@
+/*
+ * This file is part of CoAnSys project.
+ * Copyright (c) 2012-2014 ICM-UW
+ *
+ * CoAnSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CoAnSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CoAnSys. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package pl.edu.icm.coansys.citations.hashers
+
+import org.apache.hadoop.io.{NullWritable, BytesWritable}
+import org.apache.hadoop.mrunit.mapreduce.MapDriver
+import org.junit.Assert.assertEquals
+import org.testng.annotations.Test
+import pl.edu.icm.coansys.citations.data.{MarkedText, MatchableEntity}
+
+/**
+  * @author Mateusz Fedoryszak (m.fedoryszak@icm.edu.pl)
+  */
+class DocumentHashGeneratorTest {
+   @Test(groups = Array("fast"))
+   def generateTest() {
+     val entity = MatchableEntity.fromParameters(id = "1", author = "Kowalski", year = "2002", pages = "1-5")
+     val hashes = new DocumentHashGenerator().generate(entity)
+     assertEquals(Set(
+       "kowalski#2001#0#4", "kowalski#2002#0#4", "kowalski#2003#0#4",
+       "kowalski#2001#0#5", "kowalski#2002#0#5", "kowalski#2003#0#5",
+       "kowalski#2001#0#6", "kowalski#2002#0#6", "kowalski#2003#0#6",
+       "kowalski#2001#1#4", "kowalski#2002#1#4", "kowalski#2003#1#4",
+       "kowalski#2001#1#5", "kowalski#2002#1#5", "kowalski#2003#1#5",
+       "kowalski#2001#1#6", "kowalski#2002#1#6", "kowalski#2003#1#6",
+       "kowalski#2001#2#4", "kowalski#2002#2#4", "kowalski#2003#2#4",
+       "kowalski#2001#2#5", "kowalski#2002#2#5", "kowalski#2003#2#5",
+       "kowalski#2001#2#6", "kowalski#2002#2#6", "kowalski#2003#2#6"), hashes.toSet)
+   }
+}
