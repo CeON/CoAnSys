a8c7136153bac8516cf987071681d304e341b6a0
pdendek <pdendek@icm.edu.pl>
2014-04-28 03:38:17 +0200

    minor fixes

--- document-similarity/document-similarity-logic/src/main/java/pl/edu/icm/coansys/similarity/pig/udf/CosineSimilarity.java
+++ document-similarity/document-similarity-logic/src/main/java/pl/edu/icm/coansys/similarity/pig/udf/CosineSimilarity.java
@@ -30,78 +30,108 @@ import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 import org.apache.pig.impl.logicalLayer.FrontendException;
 import org.apache.pig.impl.logicalLayer.schema.Schema;
+import org.apache.pig.tools.pigstats.PigStatusReporter;
 
 import pl.edu.icm.coansys.commons.java.StackTraceExtractor;
 
 public class CosineSimilarity extends EvalFunc<Tuple> {
 
-    @Override
-    public Schema outputSchema(Schema p_input) {
-        try {
-            return Schema.generateNestedSchema(DataType.TUPLE,
-                    DataType.CHARARRAY, DataType.CHARARRAY, DataType.DOUBLE);
-        } catch (FrontendException e) {
-            throw new IllegalStateException(e);
-        }
-    }
+	private PigStatusReporter myreporter = null;
 
-    public Tuple exec(Tuple input) throws IOException {
-        if (input == null || input.size() == 0) {
-            return null;
-        }
+	@Override
+	public Schema outputSchema(Schema p_input) {
+		try {
+			return Schema.generateNestedSchema(DataType.TUPLE,
+					DataType.CHARARRAY, DataType.CHARARRAY, DataType.DOUBLE);
+		} catch (FrontendException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 
-        try {
-            String keyA = (String) input.get(0);
-            HashMap<String, Double> hmA = new HashMap<String, Double>();
-            HashSet<String> hsA = new HashSet<String>();
-            double denominatorA = 0;
-            for (Tuple t : (DataBag) input.get(1)) {
-                String word = (String) t.get(1);
-                double val = (Double) t.get(2);
-                denominatorA += val * val;
-                hmA.put(word, val);
-                hsA.add(word);
-            }
+	public Tuple exec(Tuple input) throws IOException {
+		if (input == null || input.size() == 0) {
+			return null;
+		}
 
-            String keyB = (String) input.get(2);
-            HashMap<String, Double> hmB = new HashMap<String, Double>();
-            HashSet<String> hsB = new HashSet<String>();
-            double denominatorB = 0;
-            for (Tuple t : (DataBag) input.get(3)) {
-                String word = (String) t.get(1);
-                double val = (Double) t.get(2);
-                denominatorB += val * val;
-                hmB.put(word, val);
-                hsB.add(word);
-            }
+		String keyA = null;
+		String keyB = null;
+		
+		try {
+			myreporter = PigStatusReporter.getInstance();
+			boolean reporterIsNotNull = myreporter != null;
 
-            hsA.retainAll(hsB);
+			keyA = (String) input.get(0);
+			HashMap<String, Double> hmA = new HashMap<String, Double>();
+			HashSet<String> hsA = new HashSet<String>();
+			double denominatorA = 0;
+			for (Tuple t : (DataBag) input.get(1)) {
+				try {
+					String word = (String) t.get(1);
+					double val = (Double) t.get(2);
+					denominatorA += val * val;
+					hmA.put(word, val);
+					hsA.add(word);
+				} catch (NullPointerException e) {
+					if (reporterIsNotNull) {
+						myreporter.getCounter("data error",
+								"term or tfidf is a null value! (in UDF CosineSimilarity)").increment(1);
+						log.error("for document "+keyA+" term or tfidf is a null value!");
+					}
+				}
+			}
 
-            double numerator = 0;
-            for (String s : hsA) {
-                numerator += hmA.get(s) * hmB.get(s);
-            }
+			keyB = (String) input.get(2);
+			HashMap<String, Double> hmB = new HashMap<String, Double>();
+			HashSet<String> hsB = new HashSet<String>();
+			double denominatorB = 0;
+			for (Tuple t : (DataBag) input.get(3)) {
+				try {
+					String word = (String) t.get(1);
+					double val = (Double) t.get(2);
+					denominatorB += val * val;
+					hmB.put(word, val);
+					hsB.add(word);
+				} catch (NullPointerException e) {
+					if (reporterIsNotNull) {
+						myreporter.getCounter("data error",
+								"term or tfidf are null values! (in UDF CosineSimilarity)").increment(1);
+						log.error("for document "+keyB+" term or tfidf is a null value!");
+					}
+				}
+			}
 
-            double denominator = Math.sqrt(denominatorA) * Math.sqrt(denominatorB);
-            double retVal = numerator / denominator;
+			hsA.retainAll(hsB);
 
-            if (retVal > 0) {
-                Object[] to = new Object[]{keyA, keyB, retVal};
-                return TupleFactory.getInstance().newTuple(Arrays.asList(to));
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            throw new IOException("Caught exception processing input row "+StackTraceExtractor.getStackTrace(e),e);
-        }
-    }
+			double numerator = 0;
+			for (String s : hsA) {
+				numerator += hmA.get(s) * hmB.get(s);
+			}
 
-    /*
-     * private Map<String, Double> extractToMap(Tuple input, int bagIndex, int
-     * keyIndex, int valIndex) throws ExecException { HashMap<String, Double> hm
-     * = new HashMap<String, Double>(); for (Tuple t : (DataBag)
-     * input.get(bagIndex)) { hm.put((String) t.get(keyIndex), (Double)
-     * t.get(valIndex)); } return hm;
-    }
-     */
+			double denominator = Math.sqrt(denominatorA)
+					* Math.sqrt(denominatorB);
+			double retVal = numerator / denominator;
+
+			if (retVal > 0) {
+				Object[] to = new Object[] { keyA, keyB, retVal };
+				return TupleFactory.getInstance().newTuple(Arrays.asList(to));
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			myreporter.getCounter("data error",
+					"general exception (in UDF CosineSimilarity)").increment(1);
+			log.error("for pair <"+keyA+","+keyB+"> general exception (in UDF CosineSimilarity) occurred: "+StackTraceExtractor.getStackTrace(e));
+			log.error("similarity equals 0 will be generated");
+			Object[] to = new Object[] { keyA, keyB, 0.0 };
+			return TupleFactory.getInstance().newTuple(Arrays.asList(to));
+		}
+	}
+
+	/*
+	 * private Map<String, Double> extractToMap(Tuple input, int bagIndex, int
+	 * keyIndex, int valIndex) throws ExecException { HashMap<String, Double> hm
+	 * = new HashMap<String, Double>(); for (Tuple t : (DataBag)
+	 * input.get(bagIndex)) { hm.put((String) t.get(keyIndex), (Double)
+	 * t.get(valIndex)); } return hm; }
+	 */
 }
