6658985f0409a4bc65802ffe7c887fb0507c12d8
pdendek <pdendek@icm.edu.pl>
2014-05-15 05:19:49 +0200

    bf

--- document-similarity/document-similarity-logic/src/main/pig/rank.py
+++ document-similarity/document-similarity-logic/src/main/pig/rank.py
@@ -9,17 +9,6 @@ last_val = None
 rank = 0
 interval = 1
 
-def rankFun(t,last_val,rank,interval):
-  tupleParts = t.split(',')
-  curr_val = int(tupleParts[rank_crit])
-  if last_val != curr_val:
-    rank = rank + interval
-    interval = 1
-    last_val = curr_val
-    print '\t'.join([str(rank)]+tupleParts)
-  else:
-    print '\t'.join([str(rank)]+tupleParts)
-    interval = interval + 1
 
 for line in sys.stdin:
   lineX = line.strip()
@@ -27,5 +16,15 @@ for line in sys.stdin:
     continue
   lineX = lineX[2:(len(lineX)-2)]
   for t in lineX.split('),('):
-    rankFun(t,last_val,rank,interval)
+    tupleParts = t.split(',')
+    curr_val = int(tupleParts[rank_crit])
+    if last_val != curr_val:
+      rank = rank + interval
+      interval = 1
+      last_val = curr_val
+      print '\t'.join([str(rank)]+tupleParts)
+    else:
+      print '\t'.join([str(rank)]+tupleParts)
+      interval = interval + 1
+
 
