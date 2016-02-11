9871d65bf0fec3df65171640c12b488e18fe3bf0
Łukasz Dumiszewski <l.dumiszewski@icm.edu.pl>
2016-02-11 11:49:13 +0100

    Shade plugin added + better cleanliness of Job test

--- citation-matching/citation-matching-coansys-code/src/test/java/pl/edu/icm/coansys/citations/coansys/CoansysCitationMatchingJobTest.java
+++ citation-matching/citation-matching-coansys-code/src/test/java/pl/edu/icm/coansys/citations/coansys/CoansysCitationMatchingJobTest.java
@@ -94,13 +94,7 @@ public class CoansysCitationMatchingJobTest {
         assertEquals(actualOutputDocIdPicOuts.size(), expectedOutputDocIdPicOuts.size());
     
         for (Pair<Text, BytesWritable> actualDocIdPicOut : actualOutputDocIdPicOuts) {
-            System.out.println("-----------------------BEGIN------------------------------");
-            System.out.println("actual key:" + actualDocIdPicOut.getKey().toString());
-            System.out.println("actual value: ");
-            printPicOut(actualDocIdPicOut.getValue());
             assertTrue(isInExcpectedDocIdPicOuts(expectedOutputDocIdPicOuts, actualDocIdPicOut));
-            System.out.println("-----------------------END------------------------------");
-            
         }
     
 
@@ -112,12 +106,8 @@ public class CoansysCitationMatchingJobTest {
     private boolean isInExcpectedDocIdPicOuts(List<Pair<Text, BytesWritable>> expectedOutputDocIdPicOuts, Pair<Text, BytesWritable> actualDocIdPicOut) throws Exception {
     
         for (Pair<Text, BytesWritable> expectedDocIdPicOut : expectedOutputDocIdPicOuts) {
-            System.out.println("expected key:" + expectedDocIdPicOut.getKey().toString());
-            System.out.println("expected value: ");
-            printPicOut(expectedDocIdPicOut.getValue());
             
             if (expectedDocIdPicOut.getKey().toString().equals(actualDocIdPicOut.getKey().toString())) {
-                System.out.println("...same keys");
                 PicOut expectedPicOut = PicOut.parseFrom(expectedDocIdPicOut.getValue().copyBytes());
                 PicOut actualPicOut = PicOut.parseFrom(actualDocIdPicOut.getValue().copyBytes());
                 assertPicOuts(expectedPicOut, actualPicOut);
@@ -132,9 +122,7 @@ public class CoansysCitationMatchingJobTest {
     
     private void assertPicOuts(PicOut expectedPicOut, PicOut actualPicOut) {
         assertEquals(actualPicOut.getDocId(), expectedPicOut.getDocId());
-        System.out.println("...same docid");
         assertEquals(actualPicOut.getRefsCount(), expectedPicOut.getRefsCount());
-        System.out.println("...same number of references");
         for (Reference actualRef : actualPicOut.getRefsList()) {
             assertTrue(isInExpectedRefs(expectedPicOut.getRefsList(), actualRef));
         }
@@ -159,48 +147,5 @@ public class CoansysCitationMatchingJobTest {
                ref1.getRawText().equals(ref2.getRawText()); 
     }
 
-    
-    private void printPicOut(BytesWritable bw) throws Exception {
-        PicOut picOut = PicOut.parseFrom(bw.copyBytes());
-        System.out.println("docId: " + picOut.getDocId());
-        System.out.println("references: ");
-        for (Reference ref: picOut.getRefsList()) {
-            System.out.println("---- docId: " + ref.getDocId());
-            System.out.println("---- refNum: " + ref.getRefNum());
-            System.out.println("---- rawText: " + ref.getRawText());
-        }
-    }
-    
-    /*
-actual key:50|od========18::f67eb9ee51e9e52fbb14b86acc9261ba
-actual value: 
-docId: 50|od========18::f67eb9ee51e9e52fbb14b86acc9261ba
-references: 
----- docId: 50|od========18::3a38fd978eeb55784dbd4d90c88d28da
----- refNum: 7
----- rawText: [7] G. Ghoshal, V. Zlati c, G. Caldarelli, and M. E. J. Newman. Random hypergraphs and their applications. Phys. Rev. E, 79(6):066118, 2009.
----- docId: 50|od========18::d436ddc6c0013761d58ffab1462ad3f4
----- refNum: 16
----- rawText: [16] M. E. J. Newman. Properties of highly clustered networks. Phys. Rev. E, 68(2):026121, 2003.
----- docId: 50|od=======279::c849c9f653f6cae13825e5349495a8b0
----- refNum: 33
----- rawText: [33] S. Funk and V. A. A. Jansen. Interacting epidemics on overlay networks. Phys. Rev. E, 81(3):036118, 2010.
-
-expected key:50|od========18::f67eb9ee51e9e52fbb14b86acc9261ba
-expected value: 
-docId: 50|od========18::f67eb9ee51e9e52fbb14b86acc9261ba
-references: 
----- docId: 50|od========18::d436ddc6c0013761d58ffab1462ad3f4
----- refNum: 16
----- rawText: [16] M. E. J. Newman. Properties of highly clustered networks. Phys. Rev. E, 68(2):026121, 2003.
----- docId: 50|od========18::3a38fd978eeb55784dbd4d90c88d28da
----- refNum: 7
----- rawText: [7] G. Ghoshal, V. Zlati c, G. Caldarelli, and M. E. J. Newman. Random hypergraphs and their applications. Phys. Rev. E, 79(6):066118, 2009.
----- docId: 50|od=======279::c849c9f653f6cae13825e5349495a8b0
----- refNum: 33
----- rawText: [33] S. Funk and V. A. A. Jansen. Interacting epidemics on overlay networks. Phys. Rev. E, 81(3):036118, 2010.
-     
-      
-     * */
-    
+ 
 }
