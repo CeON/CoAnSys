b26a95469a6ad010aa7b53c58760ce70fd557ad0
kura <kura@localhost>
2016-09-28 23:11:23 +0200

    bugfix cli

--- disambiguation-author/disambiguation-author-logic/src/main/scala/pl/edu/icm/coansys/disambiguation/author/scala/DisambiguationApr.scala
+++ disambiguation-author/disambiguation-author-logic/src/main/scala/pl/edu/icm/coansys/disambiguation/author/scala/DisambiguationApr.scala
@@ -87,7 +87,16 @@ object DisambiguationApr {
       // do stuff
         and_inputDocsData=config.and_inputDocsData
         and_threshold=config.and_threshold
-        and_feature_info=config.and_feature_info
+        and_feature_info= 
+         {(z:String) => { if (z.startsWith("\"")) {
+             z.substring(1)
+          }  else {
+            z
+          } }}.apply({(z:String) => { if (z.endsWith("\"")) {
+             z.substring(0, z.length-1)
+          }  else {
+            z
+          } }}.apply(and_feature_info))
         and_aproximate_remember_sim=config.and_aproximate_remember_sim
         and_use_extractor_id_instead_name=config.and_use_extractor_id_instead_name
         and_statistics=config.and_statistics
